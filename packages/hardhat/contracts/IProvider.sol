c8fe5dff38b4638a2638b3a857ceab910644caa3
Edgarillo <67366139+Edgarillo@users.noreply.github.com>
2021-01-25 15:49:47 +0100

    up
    up

--- packages/hardhat/contracts/IProvider.sol
+++ packages/hardhat/contracts/IProvider.sol
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.4.25 <0.7.0;
@@ -11,7 +12,7 @@ interface IProvider {
 
   // returns the address of the assets received from depositing a collateral;
   // for ex. for ETH as collateralAsset, AaveProvider should return the address of aWETH
-  // and Compound should return the address of cETH. 
+  // and Compound should return the address of cETH.
   //function getRedeemableAddress(address collateralAsset) external returns(address);
 
   // returns the borrow annualized rate for an asset
