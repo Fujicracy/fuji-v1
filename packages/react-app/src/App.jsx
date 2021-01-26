a406b96d5e1af7f85b2803733f609b27319e3d87
Boyan Barakov <boyan.barakov@gmail.com>
2021-01-26 10:52:24 +0100

    VAULT -> repay is functional

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -94,8 +94,8 @@ function App(props) {
   // EXTERNAL CONTRACT EXAMPLE:
   //
   // If you want to bring in the mainnet DAI contract it would look like:
-  //const mainnetDAIContract = useExternalContractLoader(mainnetProvider, DAI_ADDRESS, DAI_ABI)
-  //console.log("🥇DAI contract on mainnet:",mainnetDAIContract)
+  const mainnetDAIContract = useExternalContractLoader(mainnetProvider, DAI_ADDRESS, DAI_ABI)
+  console.log("🥇DAI contract on mainnet:",mainnetDAIContract)
   //
   // Then read your DAI balance like:
   //const myMainnetBalance = useContractReader({DAI: mainnetDAIContract},"DAI", "balanceOf",["0x34aA3F359A9D614239015126635CE7732c18fDF3"])
@@ -194,7 +194,6 @@ function App(props) {
               blockExplorer={blockExplorer}
             />
 
-            { /* Uncomment to display and interact with an external contract (DAI on mainnet):
             <Contract
               name="DAI"
               customContract={mainnetDAIContract}
@@ -203,7 +202,6 @@ function App(props) {
               address={address}
               blockExplorer={blockExplorer}
             />
-            */ }
           </Route>
           <Route path="/hints">
             <Hints
