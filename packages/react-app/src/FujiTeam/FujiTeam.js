4ebfa9a61f40346736213ebc17207039dede7dce
Edgarillo <67366139+Edgarillo@users.noreply.github.com>
2021-02-01 16:39:07 +0100

    FujiInfos + FujiTeam
    Added main components in Team and Infos Pages
    
    (also changed the comp logo)

--- packages/react-app/src/FujiTeam/FujiTeam.js
+++ packages/react-app/src/FujiTeam/FujiTeam.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Row, Col, Layout, Card } from "antd";
+import { Row, Col, Layout, Card, Image } from "antd";
 
 function FujiTeam(props) {
   const { Header, Footer, Content } = Layout;
@@ -19,31 +19,51 @@ function FujiTeam(props) {
         <Content>
           <Row gutter={16}>
             <Col span={4}>
-              <Card title="Card title" bordered={false}>
-                Card content
+              <Card title="Boyan Barakov" bordered={false}>
+                    <Image
+					      width={200}
+					      src="BoyanB.jpg"
+					/>
               </Card>
             </Col>
             <Col span={4}>
-              <Card title="Card title" bordered={false}>
-                Card content
+              <Card title="Jian Chiew" bordered={false}>
+                    <Image
+					      width={200}
+					      src="JcAwesome.png"
+					/>
               </Card>
             </Col>
             <Col span={4}>
-              <Card title="Card title" bordered={false}>
-                Card content
+              <Card title="Daigaro Cota" bordered={false}>
+                    <Image
+					      width={200}
+					      src="DaigaroC.jpg"
+					/>
               </Card>
             </Col>
              <Col span={4}>
-              <Card title="Card title" bordered={false}>
-                Card content
+              <Card title="Edgar Moreau" bordered={false}>
+                    <Image
+					      width={200}
+					      src="EdgarM.jpg"
+					/>
               </Card>
             </Col>
              <Col span={4}>
-              <Card title="Card title" bordered={false}>
-                Card content
+              <Card title="Oscar Fonseca" bordered={false}>
+                   <Image
+					      width={200}
+					      src="OscarF.png"
+					/>
               </Card>
             </Col>
-          </Row>        
+          </Row>
+
+                   <Image
+					      width={400}
+					      src="MarketMake.png"
+					/>
 
         </Content>
       </Layout>
