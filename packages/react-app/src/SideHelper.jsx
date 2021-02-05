096016d71e826f42aa5dfc0e61e58ebf1edb967c
Boyan Barakov <boyan.barakov@gmail.com>
2021-02-05 13:28:40 +0100

    UI -> move SideHelper in own file

---
+++ packages/react-app/src/SideHelper.jsx
@@ -0,0 +1,142 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { formatEther, parseEther, formatUnits, parseUnits } from "@ethersproject/units";
+import { useBalance, useContractReader, useContractLoader, useExchangePrice } from "./hooks";
+import { JsonRpcProvider } from "@ethersproject/providers";
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Avatar from '@material-ui/core/Avatar';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+
+const useSideHelperStyles = makeStyles(theme => ({
+  statsRow: {
+    display: "flex",
+    alignItems: "center",
+    margin: theme.spacing(3, 0),
+  },
+  providerBox: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "222px",
+    height: "100px",
+    marginRight: theme.spacing(5),
+    color: theme.palette.secondary.main,
+    border: "5px solid" + theme.palette.primary.main,
+    padding: theme.spacing(1, 4),
+    borderRadius: 50,
+    boxSizing: "border-box",
+    boxShadow: "15px 15px 4px rgba(0, 0, 0, 0.25)",
+  },
+  collatRatioBox: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-around",
+    minWidth: "200px",
+    height: "200px",
+    marginRight: theme.spacing(5),
+    color: theme.palette.secondary.main,
+    border: "5px solid",
+    borderRadius: "50%",
+    boxSizing: "border-box",
+    boxShadow: "inset -14px 14px 4px rgba(0, 0, 0, 0.25)",
+  },
+}));
+
+function SideHelper({ daiAmount, ethAmount, aaveRate, compoundRate }) {
+  const classes = useSideHelperStyles();
+  const mainnetProvider = new JsonRpcProvider(
+    "https://mainnet.infura.io/v3/f8481a1ed3b0466ead585fdbd71d8f95"
+  );
+  const price = useExchangePrice(mainnetProvider);
+
+  const ratio = ethAmount && daiAmount && price
+    ? (ethAmount * price / daiAmount)
+    : 0;
+
+  const aaveR = parseFloat(`${aaveRate}`) / 1e27 * 100;
+  const compoundR = parseFloat(`${compoundRate}`) / 1e27 * 100;
+
+  return (
+    <Grid
+      container
+      direction="column"
+      spacing={2}
+    >
+      <Grid item>
+        <Typography variant="h5">
+          Collaterization Ratio
+        </Typography>
+        <Box className={classes.statsRow}>
+          <Box
+            className={classes.collatRatioBox}
+            style={{ borderColor: ratio < 1.35 ? 'red' : 'blue' }}
+          >
+            <Typography component="p" variant="h4">
+              {parseFloat(ratio).toFixed(2)}
+            </Typography>
+          </Box>
+          <Typography component="span" variant="subtitle1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item>
+        <Typography variant="h5">
+          Providers
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Grid
+          container
+          direction="column"
+          spacing={2}
+        >
+          <Grid item className={classes.statsRow}>
+            <Box className={classes.providerBox}>
+              <Avatar
+                alt="Aave"
+                src="/Aave.png"
+              />
+              <Typography component="p" variant="h5">
+                {aaveR.toFixed(1)} %
+              </Typography>
+            </Box>
+            {compoundR > aaveR
+              ? (<>
+                  <CheckCircleIcon style={{ marginRight: "5px" }} />
+                  <Typography component="span" variant="subtitle1">
+                    currently using
+                  </Typography>
+                </>)
+              : '' }
+          </Grid>
+          <Grid item className={classes.statsRow}>
+            <Box className={classes.providerBox}>
+              <Avatar
+                alt="Compound"
+                src="/Compound.png"
+              />
+              <Typography component="p" variant="h5">
+                {compoundR.toFixed(1)} %
+              </Typography>
+              </Box>
+              {compoundR < aaveR
+                ? (<>
+                    <CheckCircleIcon style={{ marginRight: "5px" }} />
+                    <Typography component="span" variant="subtitle1">
+                      currently using
+                    </Typography>
+                  </>)
+                : '' }
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default SideHelper;
--- packages/react-app/src/VaultETHDAI.jsx
+++ packages/react-app/src/VaultETHDAI.jsx
@@ -1,21 +1,20 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { formatEther, parseEther, formatUnits, parseUnits } from "@ethersproject/units";
-import { useBalance, useContractReader, useContractLoader, useExchangePrice } from "./hooks";
+import { useBalance, useContractReader, useContractLoader } from "./hooks";
 import { Transactor } from "./helpers";
-import { JsonRpcProvider } from "@ethersproject/providers";
-import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import DoneOutlineOutlinedIcon from '@material-ui/icons/DoneOutlineOutlined';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import { makeStyles, createStyles } from "@material-ui/core/styles";
+
+import SideHelper from "./SideHelper";
 
 const useStyles = makeStyles(theme => 
   createStyles({
@@ -327,136 +326,6 @@ function BorrowConfirmation({ daiAmount, ethAmount, aaveRate, compoundRate }) {
         >
           Manage my position
         </Button>
-      </Grid>
-    </Grid>
-  );
-}
-
-// ====== SideHelper =======
-
-const useSideHelperStyles = makeStyles(theme => ({
-  statsRow: {
-    display: "flex",
-    alignItems: "center",
-    margin: theme.spacing(3, 0),
-  },
-  providerBox: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "222px",
-    height: "100px",
-    marginRight: theme.spacing(5),
-    color: theme.palette.secondary.main,
-    border: "5px solid" + theme.palette.primary.main,
-    padding: theme.spacing(1, 4),
-    borderRadius: 50,
-    boxSizing: "border-box",
-    boxShadow: "15px 15px 4px rgba(0, 0, 0, 0.25)",
-  },
-  collatRatioBox: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-around",
-    minWidth: "200px",
-    height: "200px",
-    marginRight: theme.spacing(5),
-    color: theme.palette.secondary.main,
-    border: "5px solid",
-    borderRadius: "50%",
-    boxSizing: "border-box",
-    boxShadow: "inset -14px 14px 4px rgba(0, 0, 0, 0.25)",
-  },
-}));
-
-function SideHelper({ daiAmount, ethAmount, aaveRate, compoundRate }) {
-  const classes = useSideHelperStyles();
-  const mainnetProvider = new JsonRpcProvider(
-    "https://mainnet.infura.io/v3/f8481a1ed3b0466ead585fdbd71d8f95"
-  );
-  const price = useExchangePrice(mainnetProvider);
-
-  const ratio = ethAmount && daiAmount && price
-    ? (ethAmount * price / daiAmount)
-    : 0;
-
-  const aaveR = parseFloat(`${aaveRate}`) / 1e27 * 100;
-  const compoundR = parseFloat(`${compoundRate}`) / 1e27 * 100;
-
-  return (
-    <Grid
-      container
-      direction="column"
-      spacing={2}
-    >
-      <Grid item>
-        <Typography variant="h5">
-          Collaterization Ratio
-        </Typography>
-        <Box className={classes.statsRow}>
-          <Box
-            className={classes.collatRatioBox}
-            style={{ borderColor: ratio < 1.35 ? 'red' : 'blue' }}
-          >
-            <Typography component="p" variant="h4">
-              {parseFloat(ratio).toFixed(2)}
-            </Typography>
-          </Box>
-          <Typography component="span" variant="subtitle1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          </Typography>
-        </Box>
-      </Grid>
-      <Grid item>
-        <Typography variant="h5">
-          Providers
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="column"
-          spacing={2}
-        >
-          <Grid item className={classes.statsRow}>
-            <Box className={classes.providerBox}>
-              <Avatar
-                alt="Aave"
-                src="/Aave.png"
-              />
-              <Typography component="p" variant="h5">
-                {aaveR.toFixed(1)} %
-              </Typography>
-            </Box>
-            {compoundR > aaveR
-              ? (<>
-                  <CheckCircleIcon style={{ marginRight: "5px" }} />
-                  <Typography component="span" variant="subtitle1">
-                    currently using
-                  </Typography>
-                </>)
-              : '' }
-          </Grid>
-          <Grid item className={classes.statsRow}>
-            <Box className={classes.providerBox}>
-              <Avatar
-                alt="Compound"
-                src="/Compound.png"
-              />
-              <Typography component="p" variant="h5">
-                {compoundR.toFixed(1)} %
-              </Typography>
-              </Box>
-              {compoundR < aaveR
-                ? (<>
-                    <CheckCircleIcon style={{ marginRight: "5px" }} />
-                    <Typography component="span" variant="subtitle1">
-                      currently using
-                    </Typography>
-                  </>)
-                : '' }
-          </Grid>
-        </Grid>
       </Grid>
     </Grid>
   );
