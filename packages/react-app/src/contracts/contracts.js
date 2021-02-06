6080cfdbe9bf954e6c844db026d03ab0b56fe16f
Boyan Barakov <boyan.barakov@gmail.com>
2021-02-06 08:56:13 +0100

    DebtToken -> rename and place in root

--- packages/react-app/src/contracts/contracts.js
+++ packages/react-app/src/contracts/contracts.js
@@ -1,1 +1,1 @@
-module.exports = ["Controller","Flasher","FujiMapping","ProviderAave","ProviderCompound","VaultETHDAI"];
+module.exports = ["Controller","DebtToken","Flasher","FujiMapping","ProviderAave","ProviderCompound","VaultETHDAI"];
