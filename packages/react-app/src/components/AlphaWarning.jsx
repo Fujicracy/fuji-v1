b5a5ec3d2efb2f383b2bc78df6f6d03e3ddcdaf8
Boyan Barakov <boyan.barakov@gmail.com>
2021-04-30 15:46:51 +0200

    alpha-warning -> shorthen + border

--- packages/react-app/src/components/AlphaWarning.jsx
+++ packages/react-app/src/components/AlphaWarning.jsx
@@ -5,12 +5,12 @@ import "./AlphaWarning.css";
 function AlphaWarning() {
 
   return (
-    <div className="alpha-warning dark-block">
+    <div className="alpha-warning">
       <div className="flask-icon">
         <img src="/flask.svg" alt="flask"/>
       </div>
       <Typography variant="body2">
-        This is an alpha version and contracts are not audited. Please, use at your own risk.
+        This is an alpha version and contracts are not audited. Use at your own risk.
       </Typography>
     </div>
   );
