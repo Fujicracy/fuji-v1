91cd4cc9d12e47412cdcd529816cd210e1f6f44f
Boyan Barakov <boyan.barakov@gmail.com>
2021-05-10 15:04:51 +0200

    init-accounts script for local testing

---
+++ packages/bots/init-accounts.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const { ethers, Wallet, Signer } = require('ethers');
+const { loadContracts } = require('./utils');
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.ETHEREUM_PROVIDER_URL);
+
+async function init() {
+  const contracts = await loadContracts(provider);
+  const user1 = provider.getSigner(1);
+  const user2 = provider.getSigner(2);
+
+  const depositAmount = ethers.utils.parseEther('1');
+  const borrowAmount = ethers.utils.parseUnits('1000', 18);
+  await contracts.VaultETHDAI.connect(user1)
+    .depositAndBorrow(depositAmount, borrowAmount, { value: depositAmount })
+    .then(console.log)
+    .catch(console.log);
+  await contracts.VaultETHDAI.connect(user2)
+    .depositAndBorrow(depositAmount, borrowAmount, { value: depositAmount })
+    .then(console.log)
+    .catch(console.log);
+}
+
+init();
