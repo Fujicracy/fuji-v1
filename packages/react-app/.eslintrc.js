2a51d92bb3042eab01f469405c03a4c8d9dbd546
Smart <sengineer330@gmail.com>
2021-05-11 23:22:13 -0400

    configure eslint, prettier and husky

--- packages/react-app/.eslintrc.js
+++ packages/react-app/.eslintrc.js
@@ -1,27 +1,54 @@
+const fs = require('fs')
+const path = require('path')
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, './.prettierrc'), 'utf8'),
+)
+
 module.exports = {
   env: {
     browser: true,
+    es6: true,
   },
-  extends: ["airbnb", 'plugin:prettier/recommended', 'prettier/react'],
-  plugins: ["babel"],
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['prettier', 'json', 'react', 'react-hooks'],
   rules: {
-    "prettier/prettier": ["error"],
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'arrow-body-style': ['error', 'always'],
+    'brace-style': ['error', '1tbs'],
+    'prettier/prettier': ['error', prettierOptions],
+    'react/forbid-prop-types': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
-   ],
-    "import/prefer-default-export": "off",
-    "prefer-destructuring": "off",
-    "prefer-template": "off",
-    "react/prop-types": "off",
-    "react/destructuring-assignment": "off",
-    "no-console": "off",
-    "jsx-a11y/accessible-emoji": ["off"]
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'prefer-destructuring': 'off',
+    'prefer-template': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'react/sort-prop-types': [
+      'error',
+      {
+        callbacksLast: true,
+      },
+    ],
+    'react/destructuring-assignment': 'off',
+    'no-console': 'off',
+    'jsx-a11y/accessible-emoji': ['off'],
+    'jsx-a11y/label-has-associated-control': ['off'],
+    'react-hooks/exhaustive-deps': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-props-no-spreading': ['off'],
   },
-};
+}
