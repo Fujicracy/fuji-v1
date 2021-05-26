a465122f16d8baadbda2efb4f2aeec7af559ec39
Smart <sengineer330@gmail.com>
2021-05-26 09:50:41 -0400

    add social links, make header has full width

--- packages/react-app/src/screens/About/index.jsx
+++ packages/react-app/src/screens/About/index.jsx
@@ -49,7 +49,7 @@ const useStyles = makeStyles(theme =>
         color: theme.palette.primary.main,
       },
     },
-    telegram: {
+    twitter: {
       fontSize: 16,
       color: theme.palette.secondary.main,
       paddingBottom: theme.spacing(5),
@@ -76,93 +76,117 @@ function Infos() {
   const classes = useStyles();
 
   return (
-    <Container className={classes.container}>
+    <>
       <Header />
+      <Container className={classes.container}>
+        <Grid container className={classes.section}>
+          <Grid item md={12} className={classes.item}>
+            <Typography className={classes.title}>Our Vision</Typography>
+            <Typography variant="subtitle1" className={classes.text}>
+              While FujiDAO is currently undergoing with initial updates and improvements by the
+              core team, our long-term vision tend towards a real Decentralized Autonomous
+              Organization, involving his community through governance mechanisms. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Sed metus magna.
+            </Typography>
+          </Grid>
 
-      <Grid container className={classes.section}>
-        <Grid item md={12} className={classes.item}>
-          <Typography className={classes.title}>Our Vision</Typography>
-          <Typography variant="subtitle1" className={classes.text}>
-            While FujiDAO is currently undergoing with initial updates and improvements by the core
-            team, our long-term vision tend towards a real Decentralized Autonomous Organization,
-            involving his community through governance mechanisms. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Sed metus magna.
-          </Typography>
+          <Grid container className={classes.item}>
+            <Grid item md={12}>
+              <Typography className={classes.title}>Core Team</Typography>
+            </Grid>
+
+            <Grid item md={4}>
+              <img src={teamDaigaroPhoto} alt="Daigaro Cota" />
+
+              <Typography className={classes.subtitle}>Daigaro Cota</Typography>
+
+              <div className={classes.meta}>
+                <Typography variant="h6">Role &nbsp;&nbsp;</Typography>
+                <a
+                  href="https://www.linkedin.com/in/daigaro-cota/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={linkedinIcon} alt="Daigaro's Linkedin" />
+                </a>
+              </div>
+              <a href="https://twitter.com/DaigaroC" target="_blank" rel="noreferrer">
+                <Typography className={classes.twitter}>@DaigaroC</Typography>
+              </a>
+            </Grid>
+
+            <Grid item md={4}>
+              <img src={teamBoyanPhoto} alt="Boyan Barakov" />
+
+              <Typography className={classes.subtitle}>Boyan Barakov</Typography>
+
+              <div className={classes.meta}>
+                <Typography variant="h6">Role &nbsp;&nbsp;</Typography>
+                <a
+                  href="https://www.linkedin.com/in/boyan-barakov-3b91891a/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={linkedinIcon} alt="Boyan's Linkedin" />
+                </a>
+              </div>
+
+              <a href="https://twitter.com/BoyanBarakov" target="_blank" rel="noreferrer">
+                <Typography className={classes.twitter}>@BoyanBarakov</Typography>
+              </a>
+            </Grid>
+
+            <Grid item md={4}>
+              <img src={teamEdgarPhoto} alt="Edgar Moreau" />
+
+              <Typography className={classes.subtitle}>Edgar Moreau</Typography>
+
+              <div className={classes.meta}>
+                <Typography variant="h6">Role in here &nbsp;&nbsp;</Typography>
+                <a
+                  href="https://www.linkedin.com/in/edgar-moreau-1b074b133/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={linkedinIcon} alt="Edgar's Linkedin" />
+                </a>
+              </div>
+
+              <a href="https://twitter.com/TheEdgarMoreau" target="_blank" rel="noreferrer">
+                <Typography className={classes.twitter}>@TheEdgarMoreau</Typography>
+              </a>
+            </Grid>
+          </Grid>
+
+          <Grid container className={classes.item}>
+            <Grid item md={12}>
+              <Typography className={classes.title}>Partners</Typography>
+            </Grid>
+
+            <Grid item md={6} className={classes.partner}>
+              <img
+                src={partnerDelphi}
+                className={classes.partnerImage}
+                alt="Partner - Delphi Digital"
+              />
+            </Grid>
+            <Grid item md={6} className={classes.partner}>
+              <img
+                src={partnerCapital}
+                className={classes.partnerImage}
+                alt="Partner - Origin Capital"
+              />
+            </Grid>
+            <Grid item md={6} className={classes.partner}>
+              <img src={partnerMaven} className={classes.partnerImage} alt="Partner - Maven 11" />
+            </Grid>
+            <Grid item md={6} className={classes.partner}>
+              <img src={partnerSpartan} className={classes.partnerImage} alt="Partner - Spartan" />
+            </Grid>
+          </Grid>
         </Grid>
-
-        <Grid container className={classes.item}>
-          <Grid item md={12}>
-            <Typography className={classes.title}>Core Team</Typography>
-          </Grid>
-
-          <Grid item md={4}>
-            <img src={teamDaigaroPhoto} alt="Daigaro Cota" />
-
-            <Typography className={classes.subtitle}>Daigaro Cota</Typography>
-
-            <div className={classes.meta}>
-              <Typography variant="h6">Role &nbsp;&nbsp;</Typography>
-              <img src={linkedinIcon} alt="Daigaro's Linkedin" />
-            </div>
-
-            <Typography className={classes.telegram}>@DaigaroC</Typography>
-          </Grid>
-
-          <Grid item md={4}>
-            <img src={teamBoyanPhoto} alt="Boyan Barakov" />
-
-            <Typography className={classes.subtitle}>Boyan Barakov</Typography>
-
-            <div className={classes.meta}>
-              <Typography variant="h6">Role &nbsp;&nbsp;</Typography>
-              <img src={linkedinIcon} alt="Boyan's Linkedin" />
-            </div>
-
-            <Typography className={classes.telegram}>@BoyanBarakov</Typography>
-          </Grid>
-
-          <Grid item md={4}>
-            <img src={teamEdgarPhoto} alt="Edgar Moreau" />
-
-            <Typography className={classes.subtitle}>Edgar Moreau</Typography>
-
-            <div className={classes.meta}>
-              <Typography variant="h6">Role in here &nbsp;&nbsp;</Typography>
-              <img src={linkedinIcon} alt="Edgar's Linkedin" />
-            </div>
-
-            <Typography className={classes.telegram}>@TheEdgarMoreau</Typography>
-          </Grid>
-        </Grid>
-
-        <Grid container className={classes.item}>
-          <Grid item md={12}>
-            <Typography className={classes.title}>Partners</Typography>
-          </Grid>
-
-          <Grid item md={6} className={classes.partner}>
-            <img
-              src={partnerDelphi}
-              className={classes.partnerImage}
-              alt="Partner - Delphi Digital"
-            />
-          </Grid>
-          <Grid item md={6} className={classes.partner}>
-            <img
-              src={partnerCapital}
-              className={classes.partnerImage}
-              alt="Partner - Origin Capital"
-            />
-          </Grid>
-          <Grid item md={6} className={classes.partner}>
-            <img src={partnerMaven} className={classes.partnerImage} alt="Partner - Maven 11" />
-          </Grid>
-          <Grid item md={6} className={classes.partner}>
-            <img src={partnerSpartan} className={classes.partnerImage} alt="Partner - Spartan" />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </>
   );
 }
 
