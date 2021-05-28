0c787ef98c25494ed1036f4558b18fd99950d06b
Boyan Barakov <boyan.barakov@gmail.com>
2021-05-28 11:30:06 +0200

    react-app -> change png to svg for partners

--- packages/react-app/src/assets/images/index.js
+++ packages/react-app/src/assets/images/index.js
@@ -1,14 +1,14 @@
 // App Icons
-import linkedinIcon from './icons/linkedin.png';
+import linkedinIcon from './icons/linkedin.svg';
 
 // App Images
 import teamBoyanPhoto from './images/team/boyan.svg';
 import teamDaigaroPhoto from './images/team/daigaro.svg';
 import teamEdgarPhoto from './images/team/edgar.svg';
-import partnerDelphi from './images/partner/delphiDigital.png';
-import partnerMaven from './images/partner/maven11.png';
-import partnerCapital from './images/partner/originCapital.png';
-import partnerSpartan from './images/partner/spartan.png';
+import partnerDelphi from './images/partner/delphiDigital.svg';
+import partnerMaven from './images/partner/maven11.svg';
+import partnerCapital from './images/partner/originCapital.svg';
+import partnerSpartan from './images/partner/spartan.svg';
 
 export {
   // Icons
