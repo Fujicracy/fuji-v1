1ea348d9c5612b63a183a536d4cc50c77f1a21b5
Smart <sengineer330@gmail.com>
2021-06-01 14:47:08 -0400

    change import path

--- packages/react-app/src/components/Header/index.jsx
+++ packages/react-app/src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { useAuth } from '../../hooks';
+import { useAuth } from 'hooks';
 
 function Header() {
   const [logout, setLogout] = useState(false);
---
+++ packages/react-app/src/components/index.js
@@ -0,0 +1,17 @@
+import AlphaWarning from './AlphaWarning';
+import CollaterizationIndicator from './CollaterizationIndicator';
+import Header from './Header';
+import Loader from './Loader';
+import PositionElement from './PositionElement';
+import ProtocolStats from './ProtocolStats';
+import ProvidersList from './ProvidersList';
+
+export {
+  AlphaWarning,
+  CollaterizationIndicator,
+  Header,
+  Loader,
+  PositionElement,
+  ProtocolStats,
+  ProvidersList,
+};
