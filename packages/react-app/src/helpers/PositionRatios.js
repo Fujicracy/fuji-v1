71111a1f73996f24ce5057204dd62dbe66c585f6
Smart <sengineer330@gmail.com>
2021-06-23 14:46:38 -0400

    fix price calculation

--- packages/react-app/src/helpers/PositionRatios.js
+++ packages/react-app/src/helpers/PositionRatios.js
@@ -17,6 +17,9 @@ export default function PositionRatios(position, price) {
   const liqPrice = debt / (collateral * liqThres);
   const ltv = debt / (collateral * price);
   const borrowLimit = Math.min(ltv * factor, 1);
+  // if (price > 0) {
+  //   debugger;
+  // }
 
   return {
     healthFactor: healthFactor || 0,
