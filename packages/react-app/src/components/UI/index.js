8f8967a612c23834c9fca49a9155e35c154ec5f6
Smart <sengineer330@gmail.com>
2021-06-30 12:18:49 -0500

    bug fixing

--- packages/react-app/src/components/UI/index.js
+++ packages/react-app/src/components/UI/index.js
@@ -1,7 +1,5 @@
 import Button from './Button';
 import TextInput from './TextInput';
-
-import Label from './Label';
 import { NavUnlisted, NavImageLink, NavTextLink } from './NavUnlisted';
 
-export { Button, TextInput, NavUnlisted, NavImageLink, NavTextLink, Label };
+export { Button, TextInput, NavUnlisted, NavImageLink, NavTextLink };
