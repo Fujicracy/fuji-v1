4aa1709a62e5ba4828ab9c6c6f1eb0b02236e749
DanWizard <tortortorented@gmail.com>
2021-07-02 18:25:45 -0500

    rates impl

--- packages/grafana/db/index.js
+++ packages/grafana/db/index.js
@@ -1,19 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/fuji");
 const EventPoint = require("./models/EventPoint");
+const RateBundle = require("./models/RateBundle");
 
-// TODO
-// const { MongoClient } = require("mongodb");
-// const uri =
-//   "mongodb+srv://team@angeldao.org@cluster0.nsht4.mongodb.net/data-fetch?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// client.connect((err) => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-module.exports = { EventPoint };
+module.exports = { EventPoint, RateBundle };
--- packages/grafana/server.js
+++ packages/grafana/server.js
@@ -1,31 +1,76 @@
 const express = require("express");
 const { scraper } = require("./scraper");
 const EPService = require("./services/EventPoints");
+const RPService = require("./services/RatePoints");
 var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
 const app = express();
 
-app.post("/query", jsonParser, async (req, res) => {
-  const targets = req.body.targets[0].data.only;
-  const lastBlock = (await EPService.lastBlock()) + 1;
-  const eventPoints = await scraper(lastBlock);
-  await EPService.addMany(eventPoints);
-  const data0 = await EPService.formatGrafana();
-  const metrics = data0.filter((e) => targets.includes(e.target));
-  console.log(metrics[0].datapoints);
-  res.send(metrics);
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://angeldao:ImWSfT8h9IFfS9Nk@cluster0.nsht4.mongodb.net/data-fetch?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.post("/search", jsonParser, async (req, res) => {
-  console.log("search");
-  res.send([]);
-});
+client.connect(async (err, db) => {
+  app.post("/query", jsonParser, async (req, res) => {
+    const ratesTargets = [
+      "DYDXUSDC",
+      "DYDXDAI",
+      "AAVEUSDC",
+      "AAVEDAI",
+      "COMPOUNDUSDC",
+      "COMPOUNDDAI",
+    ];
 
-app.get("/", async (req, res) => {
-  console.log("hit");
-  res.send(JSON.stringify([]));
-});
+    let flag = false;
 
-app.listen(4000, () => {
-  console.log("server started");
+    const targets = req.body.targets[0].data.only;
+    targets.forEach((e) => {
+      if (ratesTargets.includes(e)) {
+        flag = true;
+      }
+    });
+    if (flag) {
+      const [bundle, isOld] = await RPService.lastRow();
+      if (isOld) {
+        RPService.formatGrafana(db);
+      }
+      const metrics = bundle.rates.filter((e) => targets.includes(e.target));
+      console.log(metrics);
+      res.send(bundle.rates.filter((e) => targets.includes(e.target)));
+    } else {
+      const lastBlock = (await EPService.lastBlock()) + 1;
+      const eventPoints = await scraper(lastBlock);
+      await EPService.addMany(eventPoints);
+      const data0 = await EPService.formatGrafana();
+      const metrics = data0.filter((e) => targets.includes(e.target));
+      res.send(metrics);
+    }
+  });
+
+  app.post("/search", jsonParser, async (req, res) => {
+    console.log("search");
+    res.send([
+      "ETHUSDC-DEBT",
+      "ETHDAI-DEBT",
+      "DYDXUSDC",
+      "DYDXDAI",
+      "AAVEUSDC",
+      "AAVEDAI",
+      "COMPOUNDUSDC",
+      "COMPOUNDDAI",
+    ]);
+  });
+
+  app.get("/", async (req, res) => {
+    console.log("hit");
+    res.send(JSON.stringify([]));
+  });
+
+  app.listen(4000, () => {
+    console.log("server started");
+  });
 });
