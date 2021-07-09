899e70cf5ed7f11c63f8296c4fd1609b62437ae4
DCota <32775237+DaigaroCota@users.noreply.github.com>
2021-07-08 22:22:12 -0400

    harvest stkAave with some code cleanup

--- packages/hardhat/contracts/Vaults/VaultETHDAI.sol
+++ packages/hardhat/contracts/Vaults/VaultETHDAI.sol
@@ -17,11 +17,11 @@ import { Errors } from "../Libraries/Errors.sol";
 import "hardhat/console.sol"; //test line
 
 interface IVaultHarvester {
-  function collectRewards(uint256 _farmProtocolNum) external returns (address claimedToken);
-}
+  function getAaveClaimAddrs() external view returns (address[] calldata);
 
-interface IComptroller {
-  function claimComp(address holder) external;
+  function getAaveLM() external view returns (address);
+
+  function collectRewards(uint256 _farmProtocolNum) external returns (address claimedToken);
 }
 
 interface IAaveLiquidityMining {
@@ -32,11 +32,6 @@ interface IAaveLiquidityMining {
   ) external returns (uint256);
 
   function getUserUnclaimedRewards(address _user) external view returns (uint256);
-
-  function getRewardsBalance(address[] calldata assets, address user)
-    external
-    view
-    returns (uint256);
 }
 
 contract VaultETHDAI is IVault, VaultBase, ReentrancyGuard {
@@ -60,11 +55,6 @@ contract VaultETHDAI is IVault, VaultBase, ReentrancyGuard {
   IFujiAdmin private _fujiAdmin;
   address public override fujiERC1155;
   AggregatorV3Interface public oracle;
-
-  address[] public aaveClaimAddrs;
-  IAaveLiquidityMining private _aaVeLM =
-    IAaveLiquidityMining(0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5);
-  IComptroller private _comppTLR = IComptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
 
   modifier isAuthorized() {
     require(
@@ -90,14 +80,6 @@ contract VaultETHDAI is IVault, VaultBase, ReentrancyGuard {
     // 1.269
     collatF.a = 80;
     collatF.b = 63;
-
-    aaveClaimAddrs.push(address(0x030bA81f1c18d280636F32af80b9AAd02Cf0854e)); // aWETH
-    aaveClaimAddrs.push(address(0xF63B34710400CAd3e044cFfDcAb00a0f32E33eCf)); // variableDebtWETH
-    aaveClaimAddrs.push(address(0x6C3c78838c761c6Ac7bE9F59fe808ea2A6E4379d)); // variableDebtDAI
-    aaveClaimAddrs.push(address(0x619beb58998eD2278e08620f97007e1116D5D25b)); // variableDebtUSDC
-    aaveClaimAddrs.push(address(0x531842cEbbdD378f8ee36D171d6cC9C4fcf475Ec)); // variableDebtUSDT
-    aaveClaimAddrs.push(address(0x9ff58f4fFB29fA2266Ab25e75e2A8b3503311656)); // awBTC
-    aaveClaimAddrs.push(address(0x9c39809Dec7F95F5e0713634a4D0701329B3b4d2)); // variableDebtwBTC
   }
 
   receive() external payable {}
@@ -501,31 +483,28 @@ contract VaultETHDAI is IVault, VaultBase, ReentrancyGuard {
    * @param _farmProtocolNum: number per VaultHarvester Contract for specific farm
    */
   function harvestRewards(uint256 _farmProtocolNum) external onlyOwner {
-    address claimedToken;
+    IVaultHarvester vharvest = IVaultHarvester(_fujiAdmin.getVaultHarvester());
+    address tokenReturned;
 
-    if (_farmProtocolNum == 0) {
-      _comppTLR.claimComp(address(this));
-      claimedToken = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
-    } else if (_farmProtocolNum == 1) {
-      uint256 unclaimedrewards = _aaVeLM.getUserUnclaimedRewards(address(this));
-      console.log("value unclaimedrewards", unclaimedrewards);
-      _aaVeLM.claimRewards(aaveClaimAddrs, unclaimedrewards, address(this));
-      /*
-      bytes memory data = abi.encodeWithSignature(
-        "claimRewards(address[],uint256,address)",
-        aaveClaimAddrs,
-        unclaimedrewards,
-        address(this)
+    if (_farmProtocolNum == 1) {
+      IAaveLiquidityMining _aaVeLM = IAaveLiquidityMining(
+        0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5
       );
-      _execute(address(_aaVeLM), data);
-      */
-      claimedToken = 0x4da27a545c0c5B758a6BA100e3a049001de870f5;
-      // uint256 number = _aaVeLM.claimRewards(aaveClaimAddrs, unclaimedrewards, msg.sender);
+
+      address[] memory aaveClaimAddrs = vharvest.getAaveClaimAddrs();
+      uint256 unclaimedrewards = _aaVeLM.getUserUnclaimedRewards(address(this));
+
+      _aaVeLM.claimRewards(aaveClaimAddrs, unclaimedrewards, address(this));
+
+      tokenReturned = 0x4da27a545c0c5B758a6BA100e3a049001de870f5;
     } else {
-      claimedToken = 0x0000000000000000000000000000000000000000;
+      tokenReturned = vharvest.collectRewards(_farmProtocolNum);
     }
-    uint256 claimedTokenBal = IERC20(claimedToken).balanceOf(address(this));
-    require(claimedToken != address(0) && claimedTokenBal > 0, Errors.VL_HARVESTING_FAILED);
-    IERC20(claimedToken).uniTransfer(payable(_fujiAdmin.getTreasury()), claimedTokenBal);
+
+    uint256 tokenBal = IERC20(tokenReturned).balanceOf(address(this));
+
+    require(tokenReturned != address(0) && tokenBal > 0, Errors.VL_HARVESTING_FAILED);
+
+    IERC20(tokenReturned).uniTransfer(payable(_fujiAdmin.getTreasury()), tokenBal);
   }
 }
--- packages/hardhat/contracts/Vaults/VaultHarvester.sol
+++ packages/hardhat/contracts/Vaults/VaultHarvester.sol
@@ -17,55 +17,51 @@ interface IAaveLiquidityMining {
   ) external returns (uint256);
 
   function getUserUnclaimedRewards(address _user) external view returns (uint256);
-
-  function getRewardsBalance(address[] calldata assets, address user)
-    external
-    view
-    returns (uint256);
 }
 
 contract VaultHarvester {
   address[] public aaveClaimAddrs;
+
   IAaveLiquidityMining private _aaVeLM =
     IAaveLiquidityMining(0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5);
+
   IComptroller private _comppTLR = IComptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
 
   constructor() {
-    aaveClaimAddrs.push(address(0x030bA81f1c18d280636F32af80b9AAd02Cf0854e)); // aWETH
-    aaveClaimAddrs.push(address(0xF63B34710400CAd3e044cFfDcAb00a0f32E33eCf)); // variableDebtWETH
-    aaveClaimAddrs.push(address(0x6C3c78838c761c6Ac7bE9F59fe808ea2A6E4379d)); // variableDebtDAI
-    aaveClaimAddrs.push(address(0x619beb58998eD2278e08620f97007e1116D5D25b)); // variableDebtUSDC
-    aaveClaimAddrs.push(address(0x531842cEbbdD378f8ee36D171d6cC9C4fcf475Ec)); // variableDebtUSDT
-    aaveClaimAddrs.push(address(0x9ff58f4fFB29fA2266Ab25e75e2A8b3503311656)); // awBTC
-    aaveClaimAddrs.push(address(0x9c39809Dec7F95F5e0713634a4D0701329B3b4d2)); // variableDebtwBTC
+    aaveClaimAddrs.push(address(0x030bA81f1c18d280636F32af80b9AAd02Cf0854e));
+    aaveClaimAddrs.push(address(0xF63B34710400CAd3e044cFfDcAb00a0f32E33eCf));
+    aaveClaimAddrs.push(address(0xBcca60bB61934080951369a648Fb03DF4F96263C));
+    aaveClaimAddrs.push(address(0x619beb58998eD2278e08620f97007e1116D5D25b));
+    aaveClaimAddrs.push(address(0x028171bCA77440897B824Ca71D1c56caC55b68A3));
+    aaveClaimAddrs.push(address(0x6C3c78838c761c6Ac7bE9F59fe808ea2A6E4379d));
+    aaveClaimAddrs.push(address(0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811));
+    aaveClaimAddrs.push(address(0x531842cEbbdD378f8ee36D171d6cC9C4fcf475Ec));
+    aaveClaimAddrs.push(address(0x9ff58f4fFB29fA2266Ab25e75e2A8b3503311656));
+    aaveClaimAddrs.push(address(0x9c39809Dec7F95F5e0713634a4D0701329B3b4d2));
+  }
+
+  function getAaveClaimAddrs() external view returns (address[] memory) {
+    return aaveClaimAddrs;
+  }
+
+  function getAaveClaimableAmount(address user) external view returns (uint256) {
+    return _aaVeLM.getUserUnclaimedRewards(user);
+  }
+
+  function getAaveLM() external view returns (address) {
+    return address(_aaVeLM);
   }
 
   /**
    * @dev Called by the Vault to harvest farmed tokens at baselayer Protocols
    * @param _farmProtocolNum: Number assigned to Protocol for farming
    */
-  function collectRewards(uint256 _farmProtocolNum) external returns (bytes memory claimedToken) {
+  function collectRewards(uint256 _farmProtocolNum) external returns (address claimedToken) {
     if (_farmProtocolNum == 0) {
       _comppTLR.claimComp(msg.sender);
-      //claimedToken = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
-    } else if (_farmProtocolNum == 1) {
-      uint256 unclaimedrewards = _aaVeLM.getUserUnclaimedRewards(msg.sender);
-      console.log("who is msg.sender", msg.sender);
-      console.log("unclaimedrewards", unclaimedrewards);
-
-      bytes memory data = abi.encodeWithSignature(
-        "claimRewards(address[] calldata,uint256,address)",
-        aaveClaimAddrs,
-        unclaimedrewards,
-        msg.sender
-      );
-      bytes memory claimedToken = _execute(address(_aaVeLM), data);
-
-      // uint256 number = _aaVeLM.claimRewards(aaveClaimAddrs, unclaimedrewards, msg.sender);
-      //console.log('returned value ClainRewards', response);
-      //claimedToken = 0x4da27a545c0c5B758a6BA100e3a049001de870f5;
+      claimedToken = 0xc00e94Cb662C3520282E6f5717214004A7f26888;
     } else {
-      //claimedToken = 0x0000000000000000000000000000000000000000;
+      claimedToken = 0x0000000000000000000000000000000000000000;
     }
   }
 
