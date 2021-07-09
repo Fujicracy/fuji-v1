97a230641e9e9c40b9cf840d32d4f7493b1d70f0
Smart <sengineer330@gmail.com>
2021-07-08 22:54:05 -0500

    🎨 used new DropDown component in ProvidersList

--- packages/react-app/src/components/ProvidersList/styles.js
+++ packages/react-app/src/components/ProvidersList/styles.js
@@ -3,11 +3,26 @@ import styled from 'styled-components';
 import { themeGet } from '@styled-system/theme-get';
 import { Box } from 'rebass';
 
-export const ProviderContainer = styled(Box).attrs(props => ({
-  px: 4,
-  bg: themeGet('colors.dark56')(props),
+export const ProviderContainer = styled(Box).attrs(() => ({
+  // bg: themeGet('colors.dark56')(props),
+  bg: 'transparent',
 }))`
+  border-top: 0.063rem solid rgba(255, 255, 255, 0.05);
+  padding: 14px 0px 14px 0px;
+  margin: 0px;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: space-between;
+  color: ${themeGet('colors.text64')};
+`;
+
+export const AssetContainer = styled(Box).attrs(() => ({
+  // bg: themeGet('colors.dark56')(props),
+  bg: 'transparent',
+}))`
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: ${themeGet('colors.text64')};
 `;
