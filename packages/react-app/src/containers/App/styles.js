555768110757548373374525dba0844bcc8376eb
Smart <sengineer330@gmail.com>
2021-07-13 10:52:30 -0500

    changed App style to enable Discalimer Popup

--- packages/react-app/src/containers/App/styles.js
+++ packages/react-app/src/containers/App/styles.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 import { Box } from 'rebass';
+import { BaseModalBackground } from 'styled-react-modal';
 
 export const Container = styled(Box)`
   min-height: 100vh;
+`;
+
+export const FadingBackground = styled(BaseModalBackground)`
+  backdrop-filter: blur(8px);
+  transition: all 0.3s ease-in-out;
 `;
