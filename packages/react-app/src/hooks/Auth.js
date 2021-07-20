945d000b77ebba351936f3876cb9f322bb6038c9
Smart <sengineer330@gmail.com>
2021-07-20 08:34:51 -0500

    WIP: migrating web3modal to bnc-onboard

--- packages/react-app/src/hooks/Auth.js
+++ packages/react-app/src/hooks/Auth.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect, useCallback, useContext, createContext } from 'react';
-import { useUserAddress } from 'eth-hooks';
+// import { useUserAddress } from 'eth-hooks';
 import { Web3Provider } from '@ethersproject/providers';
+import { ethers } from 'ethers';
 import Web3Modal from 'web3modal';
 import WalletConnectProvider from '@walletconnect/web3-provider';
+import Onboard from 'bnc-onboard';
+import Web3 from 'web3';
 
 const AuthContext = createContext();
 
@@ -29,8 +32,33 @@ const web3Modal = new Web3Modal({
 // Provider hook that creates auth object and handles state
 function useProvideAuth() {
   const [provider, setProvider] = useState();
+  // const address = useUserAddress(provider);
+  const [address, setAddress] = useState();
+  let web3;
+  const onboard = Onboard({
+    dappId: process.env.REACT_APP_BNC_API_KEY, // [String] The API key created by step one above
+    networkId: Number(process.env.REACT_APP_CHAIN_ID), // [Integer] The Ethereum network ID your Dapp uses.
+    darkMode: true,
 
-  const address = useUserAddress(provider);
+    subscriptions: {
+      wallet: wallet => {
+        web3 = new Web3(wallet.provider);
+      },
+    },
+  });
+
+  async function connectAccount() {
+    const isSelected = await onboard.walletSelect();
+    if (isSelected) {
+      const isChecked = await onboard.walletCheck();
+      if (isChecked) {
+        const state = onboard.getState();
+        setAddress(state.address);
+        setProvider(new ethers.providers.Web3Provider(state.wallet.provider));
+        console.log({ state });
+      }
+    }
+  }
 
   const loadWeb3Modal = useCallback(async () => {
     const newProvider = await web3Modal.connect();
@@ -50,10 +78,15 @@ function useProvideAuth() {
     }
   }, [loadWeb3Modal]);
 
+  useEffect(() => {
+    setProvider(web3);
+  }, [web3]);
+
   // Return the user object and auth methods
   return {
     address,
     provider,
+    connectAccount,
     loadWeb3Modal,
     logoutOfWeb3Modal,
   };
