2ad7645fbbd479340a45792cf9cf7714c49810ec
Smart <sengineer330@gmail.com>
2021-07-20 20:43:49 -0500

    implemented the new UI of Header (balance, wallet)

--- packages/react-app/src/components/Header/index.jsx
+++ packages/react-app/src/components/Header/index.jsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useAuth } from 'hooks';
-import { Container, Logo, Navigation } from './styles';
+import { Image, Box } from 'rebass';
+import { downIcon } from 'assets/images';
+import { Label } from 'components/UI';
 import { LANDING_URL } from '../../consts/providers';
+import {
+  Container,
+  Logo,
+  Navigation,
+  BallanceContainer,
+  WalletItemContainer,
+  WalletHeader,
+  WalletItem,
+} from './styles';
 
 function Header() {
   const [logout, setLogout] = useState(false);
+  const [isOpenWallet, setIsOpenWallet] = useState(false);
+
   const { address, loadWeb3Modal, logoutOfWeb3Modal } = useAuth();
 
+  const ellipsedAddress = address ? address.substr(0, 6) + '...' + address.substr(-4, 4) : '';
+
+  console.log({ isOpenWallet });
   return (
     <Container>
       <a href={LANDING_URL} className="logo">
@@ -29,6 +45,32 @@ function Header() {
           </li>
 
           <li>
+            <BallanceContainer rightPadding={0} onBlur={() => setIsOpenWallet(false)}>
+              <Label color="#f5f5f5">123412344.23 ETH</Label>
+              <Box
+                ml={2}
+                sx={{ position: 'relative' }}
+                tabIndex="0"
+                onBlur={() => setIsOpenWallet(false)}
+              >
+                <WalletHeader
+                  onClick={() => setIsOpenWallet(!isOpenWallet)}
+                  isClicked={isOpenWallet}
+                >
+                  <Label color="#f5f5f5">{ellipsedAddress}</Label>
+                  <Image src={downIcon} ml={2} />
+                </WalletHeader>
+                {isOpenWallet && (
+                  <WalletItemContainer>
+                    <WalletItem>Change Wallet</WalletItem>
+                    <WalletItem>Disconnect</WalletItem>
+                  </WalletItemContainer>
+                )}
+              </Box>
+            </BallanceContainer>
+          </li>
+
+          <li>
             <a
               href="/"
               onClick={() => (!address ? loadWeb3Modal() : logoutOfWeb3Modal())}
@@ -36,11 +78,7 @@ function Header() {
               onMouseLeave={() => setLogout(false)}
               className={address ? 'button-nav connected' : 'button-nav'}
             >
-              {!address
-                ? 'Connect Wallet'
-                : logout
-                ? 'Disconnect'
-                : address.substr(0, 6) + '...' + address.substr(-4, 4)}
+              {!address ? 'Connect Wallet' : logout ? 'Disconnect' : ellipsedAddress}
             </a>
           </li>
         </Navigation>
