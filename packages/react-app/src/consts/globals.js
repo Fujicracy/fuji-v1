d888c6c5df827ed307fb66616d08624c9502ee97
Smart <sengineer330@gmail.com>
2021-08-09 14:16:37 -0400

    added default values for global variables

--- packages/react-app/src/consts/globals.js
+++ packages/react-app/src/consts/globals.js
@@ -6,10 +6,10 @@ const NETWORKS = {
 
 const ETH_CAP_VALUE = 2;
 
-const CHAIN_ID = process.env.REACT_APP_CHAIN_ID;
+const CHAIN_ID = process.env.REACT_APP_CHAIN_ID || 31337;
 const NETWORK = NETWORKS[CHAIN_ID];
-const APP_URL = process.env.REACT_APP_APP_URL;
-const LANDING_URL = process.env.REACT_APP_LANDING_URL;
+const APP_URL = process.env.REACT_APP_APP_URL || 'http://localhost:3000';
+const LANDING_URL = process.env.REACT_APP_LANDING_URL || 'http://localhost:3000';
 const INFURA_ID = process.env.REACT_APP_INFURA_ID;
 
 export { CHAIN_ID, NETWORK, APP_URL, LANDING_URL, INFURA_ID, ETH_CAP_VALUE };
