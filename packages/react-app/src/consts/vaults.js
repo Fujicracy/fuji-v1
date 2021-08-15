0490242a795ec4df5582ec8aa3e231537562ebcd
Boyan Barakov <boyan.barakov@gmail.com>
2021-08-15 10:29:10 +0200

    react-app -> core vaults and fuse vaults

--- packages/react-app/src/consts/vaults.js
+++ packages/react-app/src/consts/vaults.js
@@ -1,67 +1,13 @@
-import { getContractAddress } from 'helpers';
-import { ASSETS, ASSET_NAME } from './assets';
-import { PROVIDER_TYPE, PROVIDERS } from './providers';
+import { DEPLOYMENT } from 'consts/globals';
+import * as core from './vaults-core';
+import * as fuse from './vaults-fuse';
 
-const VAULTS_ADDRESS = {
-  VaultETHDAI: getContractAddress('VaultETHDAI'),
-  VaultETHUSDC: getContractAddress('VaultETHUSDC'),
-  VaultETHUSDT: getContractAddress('VaultETHUSDT'),
-};
+const VAULTS_ADDRESS = DEPLOYMENT === 'core' ? core.VAULTS_ADDRESS : fuse.VAULTS_ADDRESS;
 
-const COLLATERAL_IDS = {
-  [VAULTS_ADDRESS.VaultETHDAI]: 0,
-  [VAULTS_ADDRESS.VaultETHUSDC]: 2,
-  [VAULTS_ADDRESS.VaultETHUSDT]: 4,
-};
+const BORROW_IDS = DEPLOYMENT === 'core' ? core.BORROW_IDS : fuse.BORROW_IDS;
 
-const BORROW_IDS = {
-  [VAULTS_ADDRESS.VaultETHDAI]: 1,
-  [VAULTS_ADDRESS.VaultETHUSDC]: 3,
-  [VAULTS_ADDRESS.VaultETHUSDT]: 5,
-};
+const COLLATERAL_IDS = DEPLOYMENT === 'core' ? core.COLLATERAL_IDS : fuse.COLLATERAL_IDS;
 
-const VAULTS = {
-  [VAULTS_ADDRESS.VaultETHDAI]: {
-    borrowAsset: ASSETS[ASSET_NAME.DAI],
-    collateralAsset: ASSETS[ASSET_NAME.ETH],
-    borrowId: BORROW_IDS[VAULTS_ADDRESS.VaultETHDAI],
-    collateralId: COLLATERAL_IDS[VAULTS_ADDRESS.VaultETHDAI],
-    providers: [
-      PROVIDERS[PROVIDER_TYPE.AAVE],
-      PROVIDERS[PROVIDER_TYPE.COMPOUND],
-      PROVIDERS[PROVIDER_TYPE.DYDX],
-      PROVIDERS[PROVIDER_TYPE.IRONBANK],
-    ],
-    name: 'VaultETHDAI',
-    title: 'ETH-DAI',
-  },
-  [VAULTS_ADDRESS.VaultETHUSDC]: {
-    borrowAsset: ASSETS[ASSET_NAME.USDC],
-    collateralAsset: ASSETS[ASSET_NAME.ETH],
-    borrowId: BORROW_IDS[VAULTS_ADDRESS.VaultETHUSDC],
-    collateralId: COLLATERAL_IDS[VAULTS_ADDRESS.VaultETHUSDC],
-    providers: [
-      PROVIDERS[PROVIDER_TYPE.AAVE],
-      PROVIDERS[PROVIDER_TYPE.COMPOUND],
-      PROVIDERS[PROVIDER_TYPE.DYDX],
-      PROVIDERS[PROVIDER_TYPE.IRONBANK],
-    ],
-    name: 'VaultETHUSDC',
-    title: 'ETH-USDC',
-  },
-  [VAULTS_ADDRESS.VaultETHUSDT]: {
-    borrowAsset: ASSETS[ASSET_NAME.USDT],
-    collateralAsset: ASSETS[ASSET_NAME.ETH],
-    borrowId: BORROW_IDS[VAULTS_ADDRESS.VaultETHUSDT],
-    collateralId: COLLATERAL_IDS[VAULTS_ADDRESS.VaultETHUSDT],
-    providers: [
-      PROVIDERS[PROVIDER_TYPE.AAVE],
-      PROVIDERS[PROVIDER_TYPE.COMPOUND],
-      PROVIDERS[PROVIDER_TYPE.IRONBANK],
-    ],
-    name: 'VaultETHUSDT',
-    title: 'ETH-USDT',
-  },
-};
+const VAULTS = DEPLOYMENT === 'core' ? core.VAULTS : fuse.VAULTS;
 
 export { VAULTS_ADDRESS, BORROW_IDS, COLLATERAL_IDS, VAULTS };
