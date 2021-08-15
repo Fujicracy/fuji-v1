63bc1214dbde5e306d2347849f8c249b2321d7e7
Boyan Barakov <boyan.barakov@gmail.com>
2021-08-15 09:58:39 +0200

    react-app -> get asset price with oracle

--- packages/react-app/src/hooks/ExchangePrice.js
+++ packages/react-app/src/hooks/ExchangePrice.js
@@ -1,36 +1,15 @@
 import { useState, useEffect } from 'react';
-import { JsonRpcProvider } from '@ethersproject/providers';
-// import { Token, WETH, Fetcher, Route } from "@uniswap/sdk";
-// import { usePoller } from "eth-hooks";
 import { formatUnits } from '@ethersproject/units';
+import { useAuth } from 'hooks';
 import { CHAINLINK_ABI } from 'consts/addresses';
+import { ASSETS } from 'consts/assets';
 import useExternalContractLoader from './ExternalContractLoader';
 
-const provider = new JsonRpcProvider(
-  `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`,
-);
-
 export default function useExchangePrice(asset = 'ETH') {
+  const { provider } = useAuth();
+
   const [price, setPrice] = useState(0);
-  let priceFeedProxy;
-
-  switch (asset) {
-    case 'DAI':
-      priceFeedProxy = '0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9';
-      break;
-
-    case 'USDC':
-      priceFeedProxy = '0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6';
-      break;
-
-    case 'USDT':
-      priceFeedProxy = '0x3E7d1eAB13ad0104d2750B8863b489D65364e32D';
-      break;
-
-    default:
-      // ETH
-      priceFeedProxy = '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419';
-  }
+  const priceFeedProxy = ASSETS[asset].oracle;
 
   const oracle = useExternalContractLoader(provider, priceFeedProxy, CHAINLINK_ABI);
 
@@ -47,21 +26,6 @@ export default function useExchangePrice(asset = 'ETH') {
     }
     fetchData();
   }, [oracle]);
-
-  // const pollPrice = () => {
-  // async function getPrice() {
-  // const DAI = new Token(
-  // mainnetProvider.network ? mainnetProvider.network.chainId : 1,
-  // "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-  // 18,
-  // );
-  // const pair = await Fetcher.fetchPairData(DAI, WETH[DAI.chainId], mainnetProvider);
-  // const route = new Route([pair], WETH[DAI.chainId]);
-  // setPrice(parseFloat(route.midPrice.toSignificant(6)));
-  // }
-  // getPrice();
-  // };
-  // usePoller(pollPrice, pollTime || 9777);
 
   return price;
 }
