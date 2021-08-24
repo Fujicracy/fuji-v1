52c9b1832256a7d04099f2d119a3a425836385f4
Smart <sengineer330@gmail.com>
2021-08-24 14:58:28 -0400

    fixed the vault address bug and UI layout

--- packages/react-app/src/screens/Dashboard/ManagePosition/index.jsx
+++ packages/react-app/src/screens/Dashboard/ManagePosition/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import ArrowBackIosOutlinedIcon from '@material-ui/icons/ArrowBackIosOutlined';
-import { VAULTS } from 'consts';
+import { useMediaQuery } from 'react-responsive';
+import { Flex } from 'rebass';
+import { Grid } from '@material-ui/core';
+
+import { VAULTS, BREAKPOINTS, BREAKPOINT_NAMES } from 'consts';
 import { useContractReader } from '../../../hooks';
 
 import FlashClose from '../FlashClose';
@@ -16,14 +20,15 @@ import ProvidersList from '../../../components/ProvidersList';
 import './styles.css';
 
 function ManagePosition({ contracts, provider, address }) {
-  const defaultVault = Object.values(VAULTS)[0];
+  // const defaultVault = Object.values(VAULTS)[0];
 
   const queries = new URLSearchParams(useLocation().search);
+  const isMobile = useMediaQuery({ maxWidth: BREAKPOINTS[BREAKPOINT_NAMES.MOBILE].inNumber });
 
   // const [actionsType, setActionsType] = useState('single');
   const actionsType = 'single';
 
-  const vaultAddress = queries?.get('vaultAddress')?.toLowerCase() || defaultVault?.address;
+  const vaultAddress = queries?.get('vaultAddress')?.toLowerCase() || Object.keys(VAULTS)[0];
 
   const vault = VAULTS[vaultAddress];
   const borrowAssetName = vault?.borrowAsset.name;
@@ -43,95 +48,107 @@ function ManagePosition({ contracts, provider, address }) {
   };
 
   return (
-    <div className="container">
-      <div className="left-content">
-        <div className="positions manage-position">
-          <div className="section-title">
-            <Link to="my-positions" className="back-to-link">
-              <span className="back-icon">
-                <ArrowBackIosOutlinedIcon />
-              </span>
-              <h3>Back to my positions</h3>
-            </Link>
-          </div>
+    <Flex flexDirection="row" alignItems="center" justifyContent="center">
+      <Grid container className="positions-container" spacing={isMobile ? 2 : 6}>
+        <Grid item md={8} sm={8}>
+          <div className="positions manage-position">
+            <div className="section-title">
+              <Link to="my-positions" className="back-to-link">
+                <span className="back-icon">
+                  <ArrowBackIosOutlinedIcon />
+                </span>
+                <h3>Back to my positions</h3>
+              </Link>
+            </div>
 
-          <div className="position-board">
-            <div className="manage-my-position one-position">
-              <PositionElement actionType={PositionActions.None} position={position} />
+            <div className="position-board">
+              <div className="manage-my-position one-position">
+                <PositionElement actionType={PositionActions.None} position={position} />
 
-              <div className="manage-settings">
-                {/* <div className="manage-mode">
-                  <div className="toggle-mode">
-                    <div className="button">
-                      <input
-                        onChange={({ target }) => setActionsType(target.checked ? 'combo' : 'single')}
-                        type="checkbox"
-                        className="checkbox"
-                      />
-                      <div className="knobs">
-                        <span className="toggle-options" data-toggle="Combo">
-                          <span>Single</span>
-                        </span>
+                <div className="manage-settings">
+                  {/* <div className="manage-mode">
+                    <div className="toggle-mode">
+                      <div className="button">
+                        <input
+                          onChange={({ target }) => setActionsType(target.checked ? 'combo' : 'single')}
+                          type="checkbox"
+                          className="checkbox"
+                        />
+                        <div className="knobs">
+                          <span className="toggle-options" data-toggle="Combo">
+                            <span>Single</span>
+                          </span>
+                        </div>
+                        <div className="layer"></div>
                       </div>
-                      <div className="layer"></div>
                     </div>
-                  </div>
-                </div> */}
+                  </div> */}
 
-                <form noValidate>
-                  <div className="manage-content">
-                    <div className="col-50">
-                      {actionsType === 'single' ? (
-                        <CollateralForm
-                          contracts={contracts}
-                          provider={provider}
-                          address={address}
-                          position={position}
-                        />
-                      ) : (
-                        <SupplyAndBorrowForm
-                          contracts={contracts}
-                          provider={provider}
-                          address={address}
-                          position={position}
-                        />
-                      )}
+                  <form noValidate>
+                    <div className="manage-content">
+                      <div className="col-50">
+                        {actionsType === 'single' ? (
+                          <CollateralForm
+                            contracts={contracts}
+                            provider={provider}
+                            address={address}
+                            position={position}
+                          />
+                        ) : (
+                          <SupplyAndBorrowForm
+                            contracts={contracts}
+                            provider={provider}
+                            address={address}
+                            position={position}
+                          />
+                        )}
+                      </div>
+                      <div className="col-50">
+                        {actionsType === 'single' ? (
+                          <DebtForm
+                            contracts={contracts}
+                            provider={provider}
+                            address={address}
+                            position={position}
+                          />
+                        ) : (
+                          <RepayAndWithdrawForm
+                            contracts={contracts}
+                            provider={provider}
+                            address={address}
+                            position={position}
+                          />
+                        )}
+                      </div>
                     </div>
-                    <div className="col-50">
-                      {actionsType === 'single' ? (
-                        <DebtForm
-                          contracts={contracts}
-                          provider={provider}
-                          address={address}
-                          position={position}
-                        />
-                      ) : (
-                        <RepayAndWithdrawForm
-                          contracts={contracts}
-                          provider={provider}
-                          address={address}
-                          position={position}
-                        />
-                      )}
-                    </div>
-                  </div>
-                </form>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <FlashClose
-          position={position}
-          contracts={contracts}
-          provider={provider}
-          address={address}
-        />
-      </div>
-      <div className="right-content">
-        <CollaterizationIndicator position={position} />
-        <ProvidersList contracts={contracts} markets={[borrowAssetName]} isSelectable={false} />
-      </div>
-    </div>
+          <FlashClose
+            position={position}
+            contracts={contracts}
+            provider={provider}
+            address={address}
+          />
+        </Grid>
+        <Grid item md={4} sm={4}>
+          <Grid container direction="column" spacing={isMobile ? 2 : 6}>
+            <Grid item>
+              <CollaterizationIndicator position={position} />
+            </Grid>
+            <Grid item>
+              <ProvidersList
+                contracts={contracts}
+                markets={[borrowAssetName]}
+                isSelectable={false}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Flex>
   );
 }
 
