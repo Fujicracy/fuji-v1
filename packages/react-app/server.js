f0f1dceccfcefc1f377b538332f3f6d8c4a4c017
Boyan Barakov <boyan.barakov@gmail.com>
2021-08-31 09:47:40 +0300

    react-app -> server.js remove obosolate route

--- packages/react-app/server.js
+++ packages/react-app/server.js
@@ -1,33 +1,33 @@
 const path = require('path');
 const express = require('express');
-const { Storage } = require('@google-cloud/storage');
-const contractList = require('./src/contracts/contracts.js');
+//const { Storage } = require('@google-cloud/storage');
+//const contractList = require('./src/contracts/contracts.js');
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
 
-// Creates a client
-const storage = new Storage();
-const bucket = storage.bucket('fuji-mainnet-eth');
+//// Creates a client
+//const storage = new Storage();
+//const bucket = storage.bucket('fuji-mainnet-eth');
 
-app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  return next();
-});
+//app.use((req, res, next) => {
+  //if (req.headers['x-forwarded-proto'] !== 'https') {
+    //return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  //}
+  //return next();
+//});
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/contracts-data', async (req, res) => {
-  const fileName = `contracts/${req.query.name}.${req.query.type}.js`;
-  const file = await bucket.file(fileName).download();
-  const str = Buffer.from(file[0], 'base64').toString();
+//app.get('/contracts-data', async (req, res) => {
+  //const fileName = `contracts/${req.query.name}.${req.query.type}.js`;
+  //const file = await bucket.file(fileName).download();
+  //const str = Buffer.from(file[0], 'base64').toString();
 
-  // remove "module.exports = " at the beginning and ";" at the end
-  const data = str.substring(17, str.length - 1).replace(/\s/g, '');
-  res.send(JSON.parse(data));
-});
+  //// remove "module.exports = " at the beginning and ";" at the end
+  //const data = str.substring(17, str.length - 1).replace(/\s/g, '');
+  //res.send(JSON.parse(data));
+//});
 
 app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
