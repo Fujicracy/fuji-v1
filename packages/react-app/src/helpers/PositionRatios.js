005f5276c0407ec8e4aad3e1fd5b6749744eae2c
Smart <sengineer330@gmail.com>
2021-10-21 06:17:37 -0500

    changed the formula

--- packages/react-app/src/helpers/PositionRatios.js
+++ packages/react-app/src/helpers/PositionRatios.js
@@ -16,7 +16,7 @@ export default function PositionRatios(position, collateralPrice, borrowPrice) {
   const healthFactor =
     debt === 0 ? Infinity : (collateral * collateralPrice * liqThres) / (debt * borrowPrice);
   const maxFactor = collateral * collateralPrice * liqThres;
-  const liqPrice = (debt * borrowPrice) / (collateral * collateralPrice * liqThres);
+  const liqPrice = (debt * borrowPrice) / (collateral * liqThres);
   const ltv = (debt * borrowPrice) / (collateral * collateralPrice);
   const borrowLimit = Math.min(ltv * factor, 1);
   // if (price > 0) {
