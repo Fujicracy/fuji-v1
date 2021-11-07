016a2665b210ca3253a6d7405f403851d1c3d0a1
Smart <sengineer330@gmail.com>
2021-11-07 03:15:15 -0600

    implemented scrolling page

---
+++ packages/react-app/src/screens/Home/FirstComponent.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useSpring, config } from 'react-spring';
+import { Button } from 'components/UI';
+import { Grid } from '@material-ui/core';
+import { SectionTitle } from 'components';
+import { Flex, Image } from 'rebass';
+import { useMediaQuery } from 'react-responsive';
+import { BREAKPOINTS, BREAKPOINT_NAMES, APP_URL } from 'consts';
+
+import { fujiLanding, fujiLandingMobile, fujiLandingTablet } from '../../assets/images';
+
+import { HomeContainer, HomeCta } from './styles';
+
+function Home() {
+  const props = useSpring({
+    from: {
+      factor: 1,
+      opacity: 0,
+    },
+    to: { factor: 150, opacity: 1 },
+    config: { duration: 800, ...config.molasses },
+  });
+
+  const isMobile = useMediaQuery({ maxWidth: BREAKPOINTS[BREAKPOINT_NAMES.MOBILE].inNumber });
+  const isTablet = useMediaQuery({
+    minWidth: BREAKPOINTS[BREAKPOINT_NAMES.MOBILE].inNumber,
+    maxWidth: BREAKPOINTS[BREAKPOINT_NAMES.TABLET].inNumber,
+  });
+
+  const handleLearnClick = () => {
+    window.open('https://docs.fujidao.org/', '_blank');
+  };
+
+  return (
+    <Flex flexDireciton="column" justifyContent="center" alignItems="center" height="100vh">
+      <HomeContainer style={props}>
+        {isMobile && (
+          <SectionTitle fontWeight="600" fontSize="20px" m={3} mb={3}>
+            The first DeFi Borrowing Aggregator
+          </SectionTitle>
+        )}
+        <Image
+          src={isMobile ? fujiLandingMobile : isTablet ? fujiLandingTablet : fujiLanding}
+          mt={2}
+          p={!isMobile && !isTablet && '48px 40px 0px'}
+        />
+        <HomeCta container spacing={3}>
+          <Grid item xs={12} sm={6} md={6}>
+            <Button onClick={handleLearnClick} block outline fontSize="18px">
+              Learn
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6}>
+            <Button
+              block
+              color="white"
+              fontSize="18px"
+              onClick={() => {
+                window.location = `${APP_URL}/#/dashboard`;
+              }}
+            >
+              App
+            </Button>
+          </Grid>
+        </HomeCta>
+      </HomeContainer>
+    </Flex>
+  );
+}
+
+export default Home;
--- packages/react-app/src/screens/Home/index.jsx
+++ packages/react-app/src/screens/Home/index.jsx
@@ -1,71 +1,39 @@
-import React from 'react';
-import { useSpring, config } from 'react-spring';
-import { Button } from 'components/UI';
-import { Grid } from '@material-ui/core';
-import { SectionTitle } from 'components';
-import { Flex, Image } from 'rebass';
-import { useMediaQuery } from 'react-responsive';
-import { BREAKPOINTS, BREAKPOINT_NAMES, APP_URL } from 'consts';
+import React, { useState } from 'react';
 
-import { fujiLanding, fujiLandingMobile, fujiLandingTablet } from '../../assets/images';
+import ReactPageScroller from 'react-page-scroller';
 
-import { HomeContainer, HomeCta } from './styles';
+import FirstComponent from './FirstComponent';
+import SecondComponent from './SecondComponent';
+import ThirdComponent from './ThirdComponent';
+import FourthComponent from './FourthComponent';
 
-function Home() {
-  const props = useSpring({
-    from: {
-      factor: 1,
-      opacity: 0,
-    },
-    to: { factor: 150, opacity: 1 },
-    config: { duration: 800, ...config.molasses },
-  });
+import './index.css';
 
-  const isMobile = useMediaQuery({ maxWidth: BREAKPOINTS[BREAKPOINT_NAMES.MOBILE].inNumber });
-  const isTablet = useMediaQuery({
-    minWidth: BREAKPOINTS[BREAKPOINT_NAMES.MOBILE].inNumber,
-    maxWidth: BREAKPOINTS[BREAKPOINT_NAMES.TABLET].inNumber,
-  });
+const HomePage = () => {
+  const [currentPage, setCurrentPage] = useState(null);
 
-  const handleLearnClick = () => {
-    window.open('https://docs.fujidao.org/', '_blank');
+  const handlePageChange = number => {
+    setCurrentPage(number);
+  };
+
+  const handleBeforePageChange = number => {
+    console.log(number);
   };
 
   return (
-    <Flex flexDireciton="column" justifyContent="center" alignItems="center" height="100vh">
-      <HomeContainer style={props}>
-        {isMobile && (
-          <SectionTitle fontWeight="600" fontSize="20px" m={3} mb={3}>
-            The first DeFi Borrowing Aggregator
-          </SectionTitle>
-        )}
-        <Image
-          src={isMobile ? fujiLandingMobile : isTablet ? fujiLandingTablet : fujiLanding}
-          mt={2}
-          p={!isMobile && !isTablet && '48px 40px 0px'}
-        />
-        <HomeCta container spacing={3}>
-          <Grid item xs={12} sm={6} md={6}>
-            <Button onClick={handleLearnClick} block outline fontSize="18px">
-              Learn
-            </Button>
-          </Grid>
-          <Grid item xs={12} sm={6} md={6}>
-            <Button
-              block
-              color="white"
-              fontSize="18px"
-              onClick={() => {
-                window.location = `${APP_URL}/#/dashboard`;
-              }}
-            >
-              App
-            </Button>
-          </Grid>
-        </HomeCta>
-      </HomeContainer>
-    </Flex>
+    <>
+      <ReactPageScroller
+        pageOnChange={handlePageChange}
+        onBeforePageScroll={handleBeforePageChange}
+        customPageNumber={currentPage}
+      >
+        <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent />
+        <FourthComponent />
+      </ReactPageScroller>
+    </>
   );
-}
+};
 
-export default Home;
+export default HomePage;
