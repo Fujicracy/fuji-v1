ccac1a0d6a15fbe4b9a717fceab08a5878198876
Boyan Barakov <boyan.barakov@gmail.com>
2021-12-01 11:15:39 +0100

    add isMounted check to ContractLoader and COntractReader hooks

--- packages/react-app/src/hooks/ContractLoader.js
+++ packages/react-app/src/hooks/ContractLoader.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
 import { Contract } from '@ethersproject/contracts';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useAuth } from 'hooks';
 import { ASSETS } from 'consts/assets';
 import { ERC20_ABI } from 'consts/abis';
@@ -19,56 +19,63 @@ const loadContractFrom = (contracts, contractName, signer) => {
 
 export default function useContractLoader() {
   const { provider: providerOrSigner, networkId, networkName } = useAuth();
+  const isMounted = useRef(false);
 
   const [contracts, setContracts] = useState();
 
   useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
     async function loadContracts() {
-      if (providerOrSigner && networkId) {
-        try {
-          // we need to check to see if this providerOrSigner has a signer or not
-          let signer;
-          let accounts;
-          if (providerOrSigner && typeof providerOrSigner.listAccounts === 'function') {
-            accounts = await providerOrSigner.listAccounts();
-          }
-
-          if (accounts && accounts.length > 0) {
-            signer = providerOrSigner.getSigner();
-          } else {
-            signer = providerOrSigner;
-          }
-
-          const newContracts = {};
-
-          const contractsData = require(`../contracts/${networkId}-core.deployment.json`);
-          const contractList = Object.keys(contractsData);
-          for (let i = 0; i < contractList.length; i += 1) {
-            const contractName = contractList[i];
-            try {
-              newContracts[contractName] = loadContractFrom(contractsData, contractName, signer);
-            } catch (e) {
-              console.log(`ERROR: Contract ${contractName} cannot be loaded!`);
-            }
-          }
-          const assetList = Object.values(ASSETS[networkName]);
-          for (let i = 0; i < assetList.length; i += 1) {
-            const asset = assetList[i];
-            try {
-              newContracts[asset.name] = new Contract(asset.address, ERC20_ABI, signer);
-            } catch (e) {
-              console.log(`ERROR: Contract ${asset.name} cannot be loaded!`);
-            }
-          }
-          setContracts(newContracts);
-        } catch (e) {
-          console.log(e);
+      try {
+        // we need to check to see if this providerOrSigner has a signer or not
+        let signer;
+        let accounts;
+        if (providerOrSigner && typeof providerOrSigner.listAccounts === 'function') {
+          accounts = await providerOrSigner.listAccounts();
         }
-      } else {
-        setContracts();
+
+        if (accounts && accounts.length > 0) {
+          signer = providerOrSigner.getSigner();
+        } else {
+          signer = providerOrSigner;
+        }
+
+        const newContracts = {};
+
+        const contractsData = require(`../contracts/${networkId}-core.deployment.json`);
+        const contractList = Object.keys(contractsData);
+        for (let i = 0; i < contractList.length; i += 1) {
+          const contractName = contractList[i];
+          try {
+            newContracts[contractName] = loadContractFrom(contractsData, contractName, signer);
+          } catch (e) {
+            console.log(`ERROR: Contract ${contractName} cannot be loaded!`);
+          }
+        }
+        const assetList = Object.values(ASSETS[networkName]);
+        for (let i = 0; i < assetList.length; i += 1) {
+          const asset = assetList[i];
+          try {
+            newContracts[asset.name] = new Contract(asset.address, ERC20_ABI, signer);
+          } catch (e) {
+            console.log(`ERROR: Contract ${asset.name} cannot be loaded!`);
+          }
+        }
+        if (isMounted.current) {
+          setContracts(newContracts);
+        }
+      } catch (e) {
+        console.log(e);
       }
     }
-    loadContracts();
+    if (providerOrSigner && networkId) loadContracts();
   }, [providerOrSigner, networkId, networkName]);
 
   return contracts;
