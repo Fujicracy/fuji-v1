78be0a74338cea7f42a8f5c774083907a3266a7f
Smart <sengineer330@gmail.com>
2022-01-06 02:05:12 -0800

    fixed the issue on DeltaPositionRatios when mobile

--- packages/react-app/src/screens/Dashboard/DeltaPositionRatios/index.jsx
+++ packages/react-app/src/screens/Dashboard/DeltaPositionRatios/index.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import { useAuth } from 'hooks';
+import { useMediaQuery } from 'react-responsive';
+import { BREAKPOINTS, BREAKPOINT_NAMES } from 'consts';
+
 import { PositionRatios, getExchangePrice } from '../../../helpers';
+
+import { CustomListItem } from './styles';
 
 function DeltaPositionRatios({ vault, currentCollateral, currentDebt, newCollateral, newDebt }) {
   const { provider } = useAuth();
@@ -14,6 +18,8 @@ function DeltaPositionRatios({ vault, currentCollateral, currentDebt, newCollate
   const [borrowLimit, setLimit] = useState([]);
   const [ltv, setLtv] = useState([]);
   const [liqPrice, setLiqPrice] = useState([]);
+
+  const isMobile = useMediaQuery({ maxWidth: BREAKPOINTS[BREAKPOINT_NAMES.MOBILE].inNumber });
 
   useEffect(() => {
     async function fetchValues() {
@@ -58,41 +64,75 @@ function DeltaPositionRatios({ vault, currentCollateral, currentDebt, newCollate
 
   return (
     <List style={{ width: '100%' }}>
-      <ListItem alignItems="flex-start">
-        <ListItemText primary="Health Factor" />
-        <ListItemSecondaryAction>
-          <Typography component="span" variant="h3">
-            {formatValue(healthFactor[0], 2) + ' -> ' + formatValue(healthFactor[1], 2)}
-          </Typography>
-        </ListItemSecondaryAction>
-      </ListItem>
+      <CustomListItem alignItems="flex-start">
+        <ListItemText
+          primary="Health Factor"
+          secondary={
+            isMobile
+              ? formatValue(healthFactor[0], 2) + ' -> ' + formatValue(healthFactor[1], 2)
+              : ''
+          }
+        />
+        {!isMobile && (
+          <ListItemSecondaryAction>
+            <Typography component="span" variant="h3">
+              {formatValue(healthFactor[0], 2) + ' -> ' + formatValue(healthFactor[1], 2)}
+            </Typography>
+          </ListItemSecondaryAction>
+        )}
+      </CustomListItem>
       <Divider component="li" style={{ backgroundColor: 'rgba(255,255,255,0.12)' }} />
-      <ListItem alignItems="flex-start">
-        <ListItemText primary="Borrow Limit" />
-        <ListItemSecondaryAction>
-          <Typography component="span" variant="h3">
-            {formatValue(borrowLimit[0], 1) + ' % -> ' + formatValue(borrowLimit[1], 1) + ' %'}
-          </Typography>
-        </ListItemSecondaryAction>
-      </ListItem>
+      <CustomListItem alignItems="flex-start">
+        <ListItemText
+          primary="Borrow Limit"
+          secondary={
+            isMobile
+              ? formatValue(borrowLimit[0], 1) + ' % -> ' + formatValue(borrowLimit[1], 1) + ' %'
+              : ''
+          }
+        />
+        {!isMobile && (
+          <ListItemSecondaryAction>
+            <Typography component="span" variant="h3">
+              {formatValue(borrowLimit[0], 1) + ' % -> ' + formatValue(borrowLimit[1], 1) + ' %'}
+            </Typography>
+          </ListItemSecondaryAction>
+        )}
+      </CustomListItem>
       <Divider component="li" style={{ backgroundColor: 'rgba(255,255,255,0.12)' }} />
-      <ListItem alignItems="flex-start">
-        <ListItemText primary="Loan-to-Value" />
-        <ListItemSecondaryAction>
-          <Typography component="span" variant="h3">
-            {formatValue(ltv[0], 1) + ' % -> ' + formatValue(ltv[1], 1) + ' %'}
-          </Typography>
-        </ListItemSecondaryAction>
-      </ListItem>
+      <CustomListItem alignItems="flex-start">
+        <ListItemText
+          primary="Loan-to-Value"
+          secondary={
+            isMobile ? formatValue(ltv[0], 1) + ' % -> ' + formatValue(ltv[1], 1) + ' %' : ''
+          }
+        />
+        {!isMobile && (
+          <ListItemSecondaryAction>
+            <Typography component="span" variant="h3">
+              {formatValue(ltv[0], 1) + ' % -> ' + formatValue(ltv[1], 1) + ' %'}
+            </Typography>
+          </ListItemSecondaryAction>
+        )}
+      </CustomListItem>
       <Divider component="li" style={{ backgroundColor: 'rgba(255,255,255,0.12)' }} />
-      <ListItem alignItems="flex-start">
-        <ListItemText primary={`${vault.collateralAsset.name} liquidation price`} />
-        <ListItemSecondaryAction>
-          <Typography component="span" variant="h3">
-            {'$ ' + formatValue(liqPrice[0], 2) + ' -> $ ' + formatValue(liqPrice[1], 2)}
-          </Typography>
-        </ListItemSecondaryAction>
-      </ListItem>
+      <CustomListItem alignItems="flex-start">
+        <ListItemText
+          primary={`${vault.collateralAsset.name} liquidation price`}
+          secondary={
+            isMobile
+              ? '$' + formatValue(liqPrice[0], 2) + ' -> $' + formatValue(liqPrice[1], 2)
+              : ''
+          }
+        />
+        {!isMobile && (
+          <ListItemSecondaryAction>
+            <Typography component="span" variant="h3">
+              {'$' + formatValue(liqPrice[0], 2) + ' -> $' + formatValue(liqPrice[1], 2)}
+            </Typography>
+          </ListItemSecondaryAction>
+        )}
+      </CustomListItem>
     </List>
   );
 }
---
+++ packages/react-app/src/screens/Dashboard/DeltaPositionRatios/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { themeGet } from '@styled-system/theme-get';
+import { fujiMedia } from 'consts';
+import ListItemText from '@material-ui/core/ListItemText';
+
+export const CustomListItem = styled(ListItemText)`
+  position: relative;
+  color: ${themeGet('colors.text100')};
+  padding-left: 0px;
+  margin-top: 12px;
+  margin-bottom: 0px;
+  ${fujiMedia.lessThan('small')`
+    p {
+      color: ${themeGet('colors.text100')};
+      font-size: 16px;
+      text-align: right;
+    }
+  `}
+  ${fujiMedia.between('small', 'medium')`
+  `}
+`;
