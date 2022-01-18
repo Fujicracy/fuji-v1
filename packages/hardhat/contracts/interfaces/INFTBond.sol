3cfff32524227b6b6d81e04e99bb34938d54d78f
Tiago Neto <tiago.f.neto@gmail.com>
2022-01-18 23:16:17 +0000

    small bug fix

--- packages/hardhat/contracts/interfaces/INFTBond.sol
+++ packages/hardhat/contracts/interfaces/INFTBond.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.2;
 
 interface INFTBond {
   function isValidVault(address vault) external view returns (bool);
-  function checkStateOfPoints(address user, uint256 balanceChange, bool isPayback) external;
+  function checkStateOfPoints(address user, uint256 balanceChange, bool isPayback, uint256 decimals) external;
 }
