90828040a43fff3383c3926c47c9f3b9a187efc1
Smart <sengineer330@gmail.com>
2022-02-11 00:13:04 -0800

    used themeGet to get colors

--- packages/react-app/src/components/Description/styles.js
+++ packages/react-app/src/components/Description/styles.js
@@ -3,10 +3,10 @@ import { themeGet } from '@styled-system/theme-get';
 import { space } from 'styled-system';
 
 export const Container = styled.fieldset`
-  border: 0.15rem solid var(--text64);
+  border: 0.15rem solid ${themeGet('colors.text64')};
   border-radius: 0.5rem;
   margin: -0.5rem 0rem 2.5rem;
-  background-color: var(--blocks56);
+  background-color: ${themeGet('colors.block56')};
   backdrop-filter: blur(0.375rem);
   -webkit-backdrop-filter: blur(0.375rem);
   padding: 0.75rem 28px 2rem;
@@ -22,7 +22,7 @@ export const TitleContainer = styled.legend`
   padding: 0rem 0.5rem;
 
   margin-left: 3rem;
-  text-shadow: 0rem 0rem 0.125rem var(--text);
+  text-shadow: 0rem 0rem 0.125rem ${themeGet('colors.text100')};
 `;
 
 export const ContentContainer = styled.p`
