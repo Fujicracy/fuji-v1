b044ac0f4cb6273b339f05fe7b71bc3807b337ce
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-02-15 18:54:38 +0100

    add: climbing speed and points

--- packages/react-app/src/screens/NftGame/Profile.jsx
+++ packages/react-app/src/screens/NftGame/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { ethers } from 'ethers';
 import { useAuth, useContractLoader, useContractReader } from 'hooks';
+import { formatUnits } from '@ethersproject/units';
 
 import editSVG from 'assets/images/icons/edit.svg';
 import decorationSvg from 'assets/images/nft-game/decorations.svg';
@@ -27,36 +27,16 @@ function Profile() {
   const { address } = useAuth();
   const contracts = useContractLoader();
 
-  const debtBalance = useContractReader(contracts, 'NFTGame', 'balanceOf', [
-    address,
-    0, // TODO
-  ]);
-  const points = debtBalance ? Number(ethers.utils.formatEther(debtBalance)) : 0;
-  // console.log(points);
+  const debtBalance = useContractReader(contracts, 'NFTGame', 'balanceOf', [address, 0]);
+  const points = debtBalance ? formatUnits(debtBalance, 5) : 0;
 
-  const [, speedPerSecond] = useContractReader(contracts, 'NFTGame', 'userdata', [address]);
-  const climbingSpeed = speedPerSecond
-    ? Number(ethers.utils.formatEther(speedPerSecond)) * 60 * 60 * 24 * 7
+  const userdata = useContractReader(contracts, 'NFTGame', 'userdata', [address]);
+  const climbingSpeedPerDay = userdata[1]
+    ? formatUnits(userdata[1].toNumber(), 5) * 60 * 60 * 24
     : 0;
-  // console.log(climbingSpeed);
-
-  // TODO: Better to do it backend / game side ?
-  // TODO: At least split logic in a hook "useBoost"
-  // Question: How do I know how much boost each token is. Hardcoded ?
-  const boost4 = useContractReader(contracts, 'NFTGame', 'balanceOf', [address, 4]);
-  // const boost5 = useContractReader(contracts, 'NFTGame', 'balanceOf', [address, 5]);
-  // and so on...
+  const climbingSpeedPerWeek = climbingSpeedPerDay * 7;
 
   const boost = 0; // in percent
-  console.log(boost4);
-  console.log(boost4 && boost4.isZero());
-  // if (!boost4?.isZero()) {
-  //   boost += 5;
-  // }
-  // if (!boost5?.isZero()) {
-  //   boost += 5;
-  // }
-  // and so on...
 
   return (
     <Container>
@@ -74,7 +54,7 @@ function Profile() {
 
       <Stats>
         <div>
-          <StatsPoints>{points?.toFixed(0)}</StatsPoints>
+          <StatsPoints>{points?.toLocaleString('en-US')}</StatsPoints>
           <StatsLegend>Points</StatsLegend>
         </div>
         <div>
@@ -89,10 +69,10 @@ function Profile() {
           The climbing speed is the lorem ipsum of the dolor sit amen.
         </ClimbingSpeedText>
         <ClimbingSpeedPer>
-          <strong>{climbingSpeed?.toFixed(2)}m</strong> per week
+          <strong>{climbingSpeedPerWeek?.toLocaleString('en-US')}</strong> points per week
         </ClimbingSpeedPer>
         <ClimbingSpeedPer>
-          <strong>12,549m</strong> per day
+          <strong>{climbingSpeedPerDay?.toLocaleString('en-US')}</strong> points per day
         </ClimbingSpeedPer>
       </ClimbingSpeed>
     </Container>
