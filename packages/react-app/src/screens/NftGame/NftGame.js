9dfb309773debf3be1a2e7f88c626d307a252039
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-02-16 11:46:02 +0100

    add: hooks into separate file. Can't put them in the `hooks` golder cause `import/no-cycle` rule. IMHO false positive because of how we use import / export

---
+++ packages/react-app/src/screens/NftGame/NftGame.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useAuth, useContractLoader, useContractReader } from 'hooks';
+import { formatUnits } from '@ethersproject/units';
+
+export function usePoints() {
+  const { address } = useAuth();
+  const contracts = useContractLoader();
+  const debtBalance = useContractReader(contracts, 'NFTGame', 'balanceOf', [address, 0]);
+
+  const [points, setPoints] = useState(0);
+
+  useEffect(() => {
+    if (debtBalance) {
+      const newPoints = Number(formatUnits(debtBalance, 5));
+      setPoints(newPoints);
+    }
+  }, [debtBalance]);
+
+  return points;
+}
+
+export function useClimbingSpeed() {
+  const { address } = useAuth();
+  const contracts = useContractLoader();
+  const userdata = useContractReader(contracts, 'NFTGame', 'userdata', [address]);
+
+  const [climbingSpeed, setclimbingSpeed] = useState({
+    climbingSpeedPerDay: 0,
+    climbingSpeedPerWeek: 0,
+  });
+
+  useEffect(() => {
+    if (userdata[1]) {
+      const climbingSpeedPerDay = Number(formatUnits(userdata[1], 5)) * 60 * 60 * 24;
+      const climbingSpeedPerWeek = climbingSpeedPerDay * 7;
+      setclimbingSpeed({ climbingSpeedPerDay, climbingSpeedPerWeek });
+    }
+  }, [userdata]);
+
+  return climbingSpeed;
+}
