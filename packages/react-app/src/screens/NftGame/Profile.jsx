d19def4159f367e4af99cbba0ae1fb7c6387ac88
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-02-16 11:32:41 +0100

    add: move fetch logic into hooks

--- packages/react-app/src/screens/NftGame/Profile.jsx
+++ packages/react-app/src/screens/NftGame/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth, useContractLoader, useContractReader } from 'hooks';
 import { formatUnits } from '@ethersproject/units';
 import { Flex, Image } from 'rebass';
@@ -15,16 +15,47 @@ import {
   HorizontalLine,
 } from './styles';
 
-function Profile() {
+function usePoints() {
   const { address } = useAuth();
   const contracts = useContractLoader();
-
   const debtBalance = useContractReader(contracts, 'NFTGame', 'balanceOf', [address, 0]);
-  const points = debtBalance ? Number(formatUnits(debtBalance, 5)) : 0;
 
+  const [points, setPoints] = useState(0);
+
+  useEffect(() => {
+    if (debtBalance) {
+      const newPoints = Number(formatUnits(debtBalance, 5));
+      setPoints(newPoints);
+    }
+  }, [debtBalance]);
+
+  return points;
+}
+
+function useClimbingSpeed() {
+  const { address } = useAuth();
+  const contracts = useContractLoader();
   const userdata = useContractReader(contracts, 'NFTGame', 'userdata', [address]);
-  const climbingSpeedPerDay = userdata[1] ? Number(formatUnits(userdata[1], 5)) * 60 * 60 * 24 : 0;
-  const climbingSpeedPerWeek = climbingSpeedPerDay * 7;
+
+  const [climbingSpeed, setclimbingSpeed] = useState({
+    climbingSpeedPerDay: 0,
+    climbingSpeedPerWeek: 0,
+  });
+
+  useEffect(() => {
+    if (userdata[1]) {
+      const climbingSpeedPerDay = Number(formatUnits(userdata[1], 5)) * 60 * 60 * 24;
+      const climbingSpeedPerWeek = climbingSpeedPerDay * 7;
+      setclimbingSpeed({ climbingSpeedPerDay, climbingSpeedPerWeek });
+    }
+  }, [userdata]);
+
+  return climbingSpeed;
+}
+
+function Profile() {
+  const points = usePoints();
+  const { climbingSpeedPerDay, climbingSpeedPerWeek } = useClimbingSpeed();
 
   const boost = 0; // in percent
   const isMobile = useMediaQuery({ maxWidth: BREAKPOINTS[BREAKPOINT_NAMES.MOBILE].inNumber });
