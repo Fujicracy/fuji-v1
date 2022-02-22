6690c65a9f9db040d8f87419f34b294843b2ad9a
Smart <sengineer330@gmail.com>
2022-02-21 17:26:24 -0800

    hook function to fetch crates amounts

--- packages/react-app/src/hooks/NftGame.js
+++ packages/react-app/src/hooks/NftGame.js
@@ -1,4 +1,5 @@
 import { formatUnits } from '@ethersproject/units';
+import { CRATE_CONTRACT_IDS } from 'consts';
 import { useAuth } from './Auth';
 import { useContractLoader } from './ContractLoader';
 import { useContractReader } from './ContractReader';
@@ -20,4 +21,37 @@ export function useClimbingSpeed() {
   const climbingSpeedPerWeek = climbingSpeedPerDay * 7;
 
   return { climbingSpeedPerDay, climbingSpeedPerWeek };
+}
+
+export function useCrateCounts() {
+  const { address } = useAuth();
+  const contracts = useContractLoader();
+  console.log({ contracts });
+  const unformattedCommonCrateAmount = useContractReader(contracts, 'NFTGame', 'balanceOf', [
+    address,
+    CRATE_CONTRACT_IDS.COMMON,
+  ]);
+  const commonCrateAmount = unformattedCommonCrateAmount
+    ? Number(formatUnits(unformattedCommonCrateAmount, 5))
+    : 0;
+
+  const unformattedEpicCrateAmount = useContractReader(contracts, 'NFTGame', 'balanceOf', [
+    address,
+    CRATE_CONTRACT_IDS.COMMON,
+  ]);
+  const epicCrateAmount = unformattedEpicCrateAmount
+    ? Number(formatUnits(unformattedEpicCrateAmount, 5))
+    : 0;
+
+  const unformattedLegendaryCrateAmount = useContractReader(contracts, 'NFTGame', 'balanceOf', [
+    address,
+    CRATE_CONTRACT_IDS.COMMON,
+  ]);
+  const legendaryCrateAmount = unformattedLegendaryCrateAmount
+    ? Number(formatUnits(unformattedLegendaryCrateAmount, 5))
+    : 0;
+
+  const totalCrateAmount = commonCrateAmount + epicCrateAmount + legendaryCrateAmount;
+
+  return { commonCrateAmount, epicCrateAmount, legendaryCrateAmount, totalCrateAmount };
 }
