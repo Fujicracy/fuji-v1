89401a2a4f45c4749be7a13ecd77c32823a6706d
Smart <sengineer330@gmail.com>
2022-03-01 09:07:05 -0800

    fetched the crate prices

--- packages/react-app/src/hooks/NftGame.js
+++ packages/react-app/src/hooks/NftGame.js
@@ -1,17 +1,21 @@
 import { formatUnits } from '@ethersproject/units';
-import { CRATE_CONTRACT_IDS } from 'consts';
+import { CRATE_CONTRACT_IDS, INVENTORY_TYPE } from 'consts';
 import { useAuth } from './Auth';
 import { useContractLoader } from './ContractLoader';
 import { useContractReader } from './ContractReader';
+
+const POINTS_DECIMALS = 5;
 
 export function useProfileInfo() {
   const { address } = useAuth();
   const contracts = useContractLoader();
   const debtBalance = useContractReader(contracts, 'NFTGame', 'balanceOf', [address, 0]);
-  const points = debtBalance ? Number(formatUnits(debtBalance, 5)) : 0;
+  const points = debtBalance ? Number(formatUnits(debtBalance, POINTS_DECIMALS)) : 0;
 
   const userdata = useContractReader(contracts, 'NFTGame', 'userdata', [address]);
-  const climbingSpeedPerDay = userdata[1] ? Number(formatUnits(userdata[1], 5)) * 60 * 60 * 24 : 0;
+  const climbingSpeedPerDay = userdata[1]
+    ? Number(formatUnits(userdata[1], POINTS_DECIMALS)) * 60 * 60 * 24
+    : 0;
   const climbingSpeedPerWeek = climbingSpeedPerDay * 7;
 
   const unformattedComputeBoost = useContractReader(contracts, 'NFTInteractions', 'computeBoost', [
@@ -25,9 +29,11 @@ export function useProfileInfo() {
   return { points, climbingSpeedPerDay, climbingSpeedPerWeek, boost };
 }
 
-export function useCrateCounts() {
+export function useCratesInfo() {
   const { address } = useAuth();
   const contracts = useContractLoader();
+
+  // common crate info
   const unformattedCommonCrateAmount = useContractReader(contracts, 'NFTGame', 'balanceOf', [
     address,
     CRATE_CONTRACT_IDS.COMMON,
@@ -35,7 +41,17 @@ export function useCrateCounts() {
   const commonCrateAmount = unformattedCommonCrateAmount
     ? Number(formatUnits(unformattedCommonCrateAmount, 0))
     : 0;
+  const unformattedCommonCratePrice = useContractReader(
+    contracts,
+    'NFTInteractions',
+    'cratePrices',
+    [CRATE_CONTRACT_IDS.COMMON],
+  );
+  const commonCratePrice = unformattedCommonCratePrice
+    ? Number(formatUnits(unformattedCommonCratePrice, POINTS_DECIMALS))
+    : 0;
 
+  // epic crate info
   const unformattedEpicCrateAmount = useContractReader(contracts, 'NFTGame', 'balanceOf', [
     address,
     CRATE_CONTRACT_IDS.EPIC,
@@ -43,7 +59,14 @@ export function useCrateCounts() {
   const epicCrateAmount = unformattedEpicCrateAmount
     ? Number(formatUnits(unformattedEpicCrateAmount, 0))
     : 0;
+  const unformattedEpicCratePrice = useContractReader(contracts, 'NFTInteractions', 'cratePrices', [
+    CRATE_CONTRACT_IDS.EPIC,
+  ]);
+  const epicCratePrice = unformattedEpicCratePrice
+    ? Number(formatUnits(unformattedEpicCratePrice, POINTS_DECIMALS))
+    : 0;
 
+  // legendary crate info
   const unformattedLegendaryCrateAmount = useContractReader(contracts, 'NFTGame', 'balanceOf', [
     address,
     CRATE_CONTRACT_IDS.LEGENDARY,
@@ -51,8 +74,29 @@ export function useCrateCounts() {
   const legendaryCrateAmount = unformattedLegendaryCrateAmount
     ? Number(formatUnits(unformattedLegendaryCrateAmount, 0))
     : 0;
+  const unformattedLegendaryCratePrice = useContractReader(
+    contracts,
+    'NFTInteractions',
+    'cratePrices',
+    [CRATE_CONTRACT_IDS.LEGENDARY],
+  );
+  const legendaryCratePrice = unformattedLegendaryCratePrice
+    ? Number(formatUnits(unformattedLegendaryCratePrice, POINTS_DECIMALS))
+    : 0;
 
   const totalCrateAmount = commonCrateAmount + epicCrateAmount + legendaryCrateAmount;
 
-  return { commonCrateAmount, epicCrateAmount, legendaryCrateAmount, totalCrateAmount };
+  return {
+    amounts: {
+      [INVENTORY_TYPE.COMMON]: commonCrateAmount,
+      [INVENTORY_TYPE.EPIC]: epicCrateAmount,
+      [INVENTORY_TYPE.LEGENDARY]: legendaryCrateAmount,
+      total: totalCrateAmount,
+    },
+    prices: {
+      [INVENTORY_TYPE.COMMON]: commonCratePrice,
+      [INVENTORY_TYPE.EPIC]: epicCratePrice,
+      [INVENTORY_TYPE.LEGENDARY]: legendaryCratePrice,
+    },
+  };
 }
