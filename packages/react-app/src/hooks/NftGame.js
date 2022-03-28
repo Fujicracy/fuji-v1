c0d6c2bb799484f82a5c03ca57f3b68a627f13fd
Boyan Barakov <boyan.barakov@gmail.com>
2022-03-28 10:34:21 +0200

    outcome probabilities in hook

--- packages/react-app/src/hooks/NftGame.js
+++ packages/react-app/src/hooks/NftGame.js
@@ -33,74 +33,109 @@ export function useProfileInfo() {
   return { points, climbingSpeedPerDay, climbingSpeedPerWeek, boost, isLoading };
 }
 
+// helpers for useCratesInfo()
+
+function formatHexArray(values, decimals = 0) {
+  if (!values) return [];
+  return values.map(v => (v ? Number(formatUnits(v, decimals)) : 0));
+}
+
+function formatHexArrayArray(values, decimals = 0) {
+  if (!values) return [];
+  return values.map(v => (v ? formatHexArray(v, decimals) : []));
+}
+
+// reference:
+// https://discord.com/channels/808620015653879828/938043588951048202/955506490788163605
+function getRewardOutcomes(probabiltyIntervals, rewards) {
+  const outcomes = {};
+
+  // transform to make easier percentage calculations
+  const intervals = probabiltyIntervals.map(i => i * 10);
+
+  let key;
+  for (let i = 0; i < intervals.length; i += 1) {
+    if (i === 0) {
+      key = intervals[0].toFixed(2) + '%';
+    } else {
+      key = (intervals[i] - intervals[i - 1]).toFixed(2) + '%';
+    }
+
+    outcomes[key] = rewards[i];
+  }
+
+  // add at the end the probabilty for a card
+  if (intervals && intervals.length > 0) {
+    key = (100 - intervals[intervals.length - 1]).toFixed(2) + '%';
+    outcomes[key] = 'card';
+  }
+
+  return outcomes;
+}
+
 export function useCratesInfo() {
   const { address } = useAuth();
   const contracts = useContractLoader();
 
-  // common crate info
-  const unformattedCommonCrateAmount = useContractReader(contracts, 'NFTGame', 'balanceOf', [
-    address,
+  const unformattedBalances = useContractReader(contracts, 'NFTGame', 'balanceOfBatch', [
+    [address, address, address],
+    [CRATE_CONTRACT_IDS.COMMON, CRATE_CONTRACT_IDS.EPIC, CRATE_CONTRACT_IDS.LEGENDARY],
+  ]);
+  const crateBalances = formatHexArray(unformattedBalances, 0);
+
+  // crates prices
+  const commonPrice = useContractReader(contracts, 'NFTInteractions', 'cratePrices', [
     CRATE_CONTRACT_IDS.COMMON,
   ]);
-  const commonCrateAmount = unformattedCommonCrateAmount
-    ? Number(formatUnits(unformattedCommonCrateAmount, 0))
-    : 0;
-  const unformattedCommonCratePrice = useContractReader(
-    contracts,
-    'NFTInteractions',
-    'cratePrices',
-    [CRATE_CONTRACT_IDS.COMMON],
-  );
-  const commonCratePrice = unformattedCommonCratePrice
-    ? Number(formatUnits(unformattedCommonCratePrice, NFT_GAME_POINTS_DECIMALS))
-    : 0;
-
-  // epic crate info
-  const unformattedEpicCrateAmount = useContractReader(contracts, 'NFTGame', 'balanceOf', [
-    address,
+  const epicPrice = useContractReader(contracts, 'NFTInteractions', 'cratePrices', [
     CRATE_CONTRACT_IDS.EPIC,
   ]);
-  const epicCrateAmount = unformattedEpicCrateAmount
-    ? Number(formatUnits(unformattedEpicCrateAmount, 0))
-    : 0;
-  const unformattedEpicCratePrice = useContractReader(contracts, 'NFTInteractions', 'cratePrices', [
-    CRATE_CONTRACT_IDS.EPIC,
-  ]);
-  const epicCratePrice = unformattedEpicCratePrice
-    ? Number(formatUnits(unformattedEpicCratePrice, NFT_GAME_POINTS_DECIMALS))
-    : 0;
-
-  // legendary crate info
-  const unformattedLegendaryCrateAmount = useContractReader(contracts, 'NFTGame', 'balanceOf', [
-    address,
+  const legendaryPrice = useContractReader(contracts, 'NFTInteractions', 'cratePrices', [
     CRATE_CONTRACT_IDS.LEGENDARY,
   ]);
-  const legendaryCrateAmount = unformattedLegendaryCrateAmount
-    ? Number(formatUnits(unformattedLegendaryCrateAmount, 0))
-    : 0;
-  const unformattedLegendaryCratePrice = useContractReader(
+  const cratePrices = formatHexArray(
+    [commonPrice, epicPrice, legendaryPrice],
+    NFT_GAME_POINTS_DECIMALS,
+  );
+
+  // crates probabilty outcomes
+  const probabiltyIntervals = useContractReader(
     contracts,
     'NFTInteractions',
-    'cratePrices',
-    [CRATE_CONTRACT_IDS.LEGENDARY],
+    'getProbabilityIntervals',
+    [],
   );
-  const legendaryCratePrice = unformattedLegendaryCratePrice
-    ? Number(formatUnits(unformattedLegendaryCratePrice, NFT_GAME_POINTS_DECIMALS))
-    : 0;
-
-  const totalCrateAmount = commonCrateAmount + epicCrateAmount + legendaryCrateAmount;
+  const intervals = formatHexArray(probabiltyIntervals, NFT_GAME_POINTS_DECIMALS);
+  const commonRewards = useContractReader(contracts, 'NFTInteractions', 'getCrateRewards', [
+    CRATE_CONTRACT_IDS.COMMON,
+  ]);
+  const epicRewards = useContractReader(contracts, 'NFTInteractions', 'getCrateRewards', [
+    CRATE_CONTRACT_IDS.EPIC,
+  ]);
+  const legendaryRewards = useContractReader(contracts, 'NFTInteractions', 'getCrateRewards', [
+    CRATE_CONTRACT_IDS.LEGENDARY,
+  ]);
+  const crateRewards = formatHexArrayArray(
+    [commonRewards, epicRewards, legendaryRewards],
+    NFT_GAME_POINTS_DECIMALS,
+  );
 
   return {
     amounts: {
-      [INVENTORY_TYPE.COMMON]: commonCrateAmount,
-      [INVENTORY_TYPE.EPIC]: epicCrateAmount,
-      [INVENTORY_TYPE.LEGENDARY]: legendaryCrateAmount,
-      total: totalCrateAmount,
+      [INVENTORY_TYPE.COMMON]: crateBalances[0],
+      [INVENTORY_TYPE.EPIC]: crateBalances[1],
+      [INVENTORY_TYPE.LEGENDARY]: crateBalances[2],
+      total: crateBalances.reduce((prev, v) => prev + v, 0),
     },
     prices: {
-      [INVENTORY_TYPE.COMMON]: commonCratePrice,
-      [INVENTORY_TYPE.EPIC]: epicCratePrice,
-      [INVENTORY_TYPE.LEGENDARY]: legendaryCratePrice,
+      [INVENTORY_TYPE.COMMON]: cratePrices[0],
+      [INVENTORY_TYPE.EPIC]: cratePrices[1],
+      [INVENTORY_TYPE.LEGENDARY]: cratePrices[2],
+    },
+    outcomes: {
+      [INVENTORY_TYPE.COMMON]: getRewardOutcomes(intervals, crateRewards[0]),
+      [INVENTORY_TYPE.EPIC]: getRewardOutcomes(intervals, crateRewards[1]),
+      [INVENTORY_TYPE.LEGENDARY]: getRewardOutcomes(intervals, crateRewards[2]),
     },
   };
 }
