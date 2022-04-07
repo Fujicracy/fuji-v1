6ad1a860230f8d251d817f99a358aa57c8b9cf47
Boyan Barakov <boyan.barakov@gmail.com>
2022-04-07 18:57:40 +0200

    fix: display Climbing in header only on Fantom

--- packages/react-app/src/components/Header/index.jsx
+++ packages/react-app/src/components/Header/index.jsx
@@ -192,14 +192,16 @@ function Header() {
                   </MenuItem>
                 </NavLink>
 
-                <NavLink to="/nft-game">
-                  <MenuItem
-                    isSelected={currentPage.pathname === '/dashboard/nft-game'}
-                    onClick={() => setIsMenuOpen(false)}
-                  >
-                    Ascend
-                  </MenuItem>
-                </NavLink>
+                {networkName === CHAIN_NAMES.FANTOM && (
+                  <NavLink to="/nft-game">
+                    <MenuItem
+                      isSelected={currentPage.pathname === '/dashboard/nft-game'}
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Climbing
+                    </MenuItem>
+                  </NavLink>
+                )}
 
                 <NavLink to="/about">
                   <MenuItem
@@ -218,7 +220,7 @@ function Header() {
                   Documentation
                 </MenuItem>
 
-                {selectedChain?.name === CHAIN_NAMES.ETHEREUM && (
+                {networkName === CHAIN_NAMES.ETHEREUM && (
                   <NavLink to="/claim-nft">
                     <MenuItem
                       isSelected={currentPage.pathname === '/dashboard/claim-nft'}
@@ -285,19 +287,22 @@ function Header() {
                     </NavLink>
                   </li>
 
-                  <li className="nav-item">
-                    <NavLink to="/nft-game" activeClassName="current">
-                      Ascent
-                    </NavLink>
-                  </li>
+                  {networkName === CHAIN_NAMES.FANTOM && (
+                    <li className="nav-item">
+                      <NavLink to="/nft-game" activeClassName="current">
+                        Climbing
+                      </NavLink>
+                    </li>
+                  )}
+
+                  {networkName === CHAIN_NAMES.ETHEREUM && (
+                    <li className="nav-item">
+                      <NavLink to="/claim-nft" activeClassName="current">
+                        NFT
+                      </NavLink>
+                    </li>
+                  )}
                 </>
-              )}
-              {selectedChain?.name === CHAIN_NAMES.ETHEREUM && (
-                <li className="nav-item">
-                  <NavLink to="/claim-nft" activeClassName="current">
-                    NFT
-                  </NavLink>
-                </li>
               )}
 
               <li>
