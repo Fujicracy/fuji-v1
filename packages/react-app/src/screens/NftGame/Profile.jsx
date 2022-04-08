0930cc50b943ce6b4685e4a77468555de068d56f
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-04-08 14:22:23 +0200

    show jaibo how to change translations

--- packages/react-app/src/screens/NftGame/Profile.jsx
+++ packages/react-app/src/screens/NftGame/Profile.jsx
@@ -128,7 +128,8 @@ function Profile() {
       >
         <SectionTitle fontSize="24px">Climbing speed</SectionTitle>
         <Label fontSize="12px" color="white" textAlign="left" mt={1} mb={1} lineHeight="18px">
-          The climbing speed is the lorem ipsum of the dolor sit amen.
+          The climbing speed is the number of points that you are collecting from your open loan in
+          Fuji a week/day.
         </Label>
 
         <ClimbingSpeedPer>
