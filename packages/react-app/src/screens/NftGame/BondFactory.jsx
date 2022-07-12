35acda4771576fac984f1b158f911d6f0a806cc4
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-07-12 17:20:20 +0200

    fix: UI

--- packages/react-app/src/screens/NftGame/BondFactory.jsx
+++ packages/react-app/src/screens/NftGame/BondFactory.jsx
@@ -15,61 +15,69 @@ function Bond({ bg = 'white', color = 'black', width, months }) {
   const [isBuying, setIsBuying] = useState(false);
 
   return (
-    <Box
-      backgroundColor={bg}
-      sx={{ borderRadius: '8px' }}
-      p={3}
-      m={1}
-      textAlign="center"
-      width={width}
-    >
-      <Text color={color} fontSize="1.5rem" fontWeight="bold">
-        {months} month bond
-      </Text>
-      <Box color={color} mt={3}>
-        <Text fontSize="1.5rem" fontWeight="bold" display="inline">
-          10000
+    <Box width={width}>
+      <Box backgroundColor={bg} sx={{ borderRadius: '8px' }} p={3} m={1} textAlign="center">
+        <Text color={color} fontSize="1.5rem" fontWeight="bold">
+          {months} month bond
         </Text>
-        <Text fontSize=".8rem" display="inline">
-          {' '}
-          Meter points
-        </Text>
-      </Box>
+        <Box color={color} mt={3}>
+          <Text fontSize="1.5rem" fontWeight="bold" display="inline">
+            10000
+          </Text>
+          <Text fontSize=".8rem" display="inline">
+            {' '}
+            Meter points
+          </Text>
+        </Box>
 
-      <Flex
-        mt={isMobile ? '10px' : '16px'}
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        width="100%"
-      >
-        <Flex flexDirection="row" justifyContent="center" alignItems="center">
-          <CountButton backgroundColor={bg} color={color} onClick={() => setAmount(amount - 1)}>
-            -
-          </CountButton>
-          <AmountInput
-            value={amount}
-            type="number"
-            onChange={e => setAmount(parseInt(e.target.value, 10))}
-          />
-          <CountButton color={color} backgroundColor={bg} onClick={() => setAmount(amount + 1)}>
-            +
-          </CountButton>
-        </Flex>
-        <BuyButton mt={isMobile ? '12px' : '16px'} onClick={() => alert('not implemented')}>
-          {isBuying && (
-            <CircularProgress
-              style={{
-                width: 20,
-                height: 20,
-                marginRight: '16px',
-                color,
-              }}
+        <Flex
+          mt={isMobile ? '10px' : '16px'}
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          width="100%"
+        >
+          <Flex flexDirection="row" justifyContent="center" alignItems="center">
+            <CountButton
+              backgroundColor={bg}
+              foreColor={color}
+              onClick={() => setAmount(amount - 1)}
+            >
+              -
+            </CountButton>
+            <AmountInput
+              value={amount}
+              theme={{ buttonColor: color, foreColor: color }}
+              type="number"
+              onChange={e => setAmount(parseInt(e.target.value, 10))}
             />
-          )}
-          Buy
-        </BuyButton>
-      </Flex>
+            <CountButton
+              foreColor={color}
+              backgroundColor={bg}
+              onClick={() => setAmount(amount + 1)}
+            >
+              +
+            </CountButton>
+          </Flex>
+          <BuyButton
+            mt={isMobile ? '12px' : '16px'}
+            theme={{ foreColor: color }}
+            onClick={() => alert('not implemented')}
+          >
+            {isBuying && (
+              <CircularProgress
+                style={{
+                  width: 20,
+                  height: 20,
+                  marginRight: '16px',
+                  color,
+                }}
+              />
+            )}
+            Buy
+          </BuyButton>
+        </Flex>
+      </Box>
     </Box>
   );
 }
@@ -101,7 +109,7 @@ function BondFactory() {
         description="A bond is redeemable against token. The longer the bond, the higher the amont of tokens redeemable."
       />
 
-      <Flex mt={4}>
+      <Flex mt={4} flexWrap="wrap">
         <Bond months="3" width={[1, 1 / 3]} />
         <Bond bg="#735CDD" color="white" months="6" width={[1, 1 / 3]} />
         <Bond bg="#A5243D" color="white" months="12" width={[1, 1 / 3]} />
