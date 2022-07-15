f0b8533ca323de093295513c8d772a54aea8fb10
Tiago Neto <me@tiagofneto.com>
2022-07-15 10:18:01 +0100

    optimism network

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -28,7 +28,9 @@ const forkUrl =
       ? "https://bsc-dataseed.binance.org/"
       : network === "polygon"
         ? `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`
-        : mainnetUrl
+        : network === "optimism"
+          ? `https://opt-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`
+          : mainnetUrl
 
 //
 // Select the network you want to deploy to here:
@@ -96,6 +98,10 @@ module.exports = {
     },
     polygon: {
       url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : { mnemonic: mnemonic() },
+    },
+    optimism: {
+      url: `https://opt-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : { mnemonic: mnemonic() },
     },
   },
