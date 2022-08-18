dc82ffb5ce4a038d9329ac40b055415341c725d1
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-08-18 10:41:48 +0200

    close #204

--- packages/react-app/src/components/EthAddress/index.jsx
+++ packages/react-app/src/components/EthAddress/index.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useAuth } from 'hooks';
 
-const EthAddress = ({ address, suffix, prefix }) => {
+const EthAddress = ({ address, suffix, prefix, withEns }) => {
+  const { provider } = useAuth();
+  const [ens, setEns] = useState(null);
+  const formattedAddress = `${address.substr(0, prefix)}...${address.substr(-suffix, suffix)}`;
+
+  useMemo(() => {
+    if (address && provider && withEns) {
+      resolveEns();
+    }
+
+    async function resolveEns() {
+      try {
+        const name = await provider.lookupAddress(address);
+        setEns(name);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }, [address, provider, withEns, setEns]);
+
   if (!address) {
     return <></>;
   }
-  return (
-    <>
-      {address.substr(0, prefix)}...{address.substr(-suffix, suffix)}
-    </>
-  );
+  return ens ? <span title={address}>{ens}</span> : <>{formattedAddress}</>;
 };
 
 EthAddress.propTypes = {
   address: PropTypes.string,
   prefix: PropTypes.number,
   suffix: PropTypes.number,
+  withEns: PropTypes.bool,
 };
 
 EthAddress.defaultProps = {
   suffix: 4,
   prefix: 6,
+  withEns: false,
 };
 
 export default EthAddress;
