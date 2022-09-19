abe9bd77471f791237b86160608ef3a341ff4cec
Boyan Barakov <boyan.barakov@gmail.com>
2022-09-19 10:48:55 +0200

    feat(bots): add arbitrum

--- packages/bots/consts/providers.js
+++ packages/bots/consts/providers.js
@@ -1,6 +1,7 @@
 import * as ethereumCore from './providers-ethereum-core.js';
 import * as ethereumFuse from './providers-ethereum-fuse.js';
 import * as fantomCore from './providers-fantom-core.js';
+import * as arbitrumCore from './providers-arbitrum-core.js';
 
 const PROVIDERS = {
   ethereum: {
@@ -9,6 +10,9 @@ const PROVIDERS = {
   },
   fantom: {
     core: fantomCore,
+  },
+  arbitrum: {
+    core: arbitrumCore,
   },
 };
 
--- packages/bots/consts/vaults.js
+++ packages/bots/consts/vaults.js
@@ -1,6 +1,7 @@
 import * as ethereumCore from './vaults-ethereum-core.js';
 import * as ethereumFuse from './vaults-ethereum-fuse.js';
 import * as fantomCore from './vaults-fantom-core.js';
+import * as arbitrumCore from './vaults-arbitrum-core.js';
 
 const VAULTS = {
   ethereum: {
@@ -9,6 +10,9 @@ const VAULTS = {
   },
   fantom: {
     core: fantomCore,
+  },
+  arbitrum: {
+    core: arbitrumCore,
   },
 };
 
